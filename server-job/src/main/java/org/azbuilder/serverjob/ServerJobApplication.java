91c857884d14fef3a81b101a32e243b908edb639
Alfredo España <alfespa17@gmail.com>
2021-04-21 14:49:46 -0600

    Add server job component

---
+++ server-job/src/main/java/org/azbuilder/serverjob/ServerJobApplication.java
@@ -0,0 +1,13 @@
+package org.azbuilder.serverjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerJobApplication.class, args);
+	}
+
+}
