fe3c1d49c7ed6ecb072383fa9ac3bc00b7ed71fa
Alfredo España <alfespa17@gmail.com>
2021-05-30 17:11:11 -0600

    Enable Azure Active Directory authentication

--- api/src/main/java/org/azbuilder/api/ServerApplication.java
+++ api/src/main/java/org/azbuilder/api/ServerApplication.java
@@ -2,7 +2,11 @@ package org.azbuilder.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class ServerApplication {
 
