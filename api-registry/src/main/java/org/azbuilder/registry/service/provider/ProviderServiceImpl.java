64d66cfa95b0c8efa18a1465da6765318e505e25
Alfredo España <alfespa17@gmail.com>
2021-08-01 13:00:35 -0600

    Update postman collection with provider examples and update ProviderService with new client version 0.4.1

--- api-registry/src/main/java/org/azbuilder/registry/service/provider/ProviderServiceImpl.java
+++ api-registry/src/main/java/org/azbuilder/registry/service/provider/ProviderServiceImpl.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.azbuilder.api.client.RestClient;
 import org.azbuilder.api.client.model.organization.Organization;
 import org.azbuilder.api.client.model.organization.provider.version.Version;
-import org.azbuilder.api.client.model.organization.provider.version.file.File;
+import org.azbuilder.api.client.model.organization.provider.version.implementation.Implementation;
 import org.azbuilder.api.client.model.response.Response;
 import org.azbuilder.api.client.model.response.ResponseWithInclude;
 import org.azbuilder.registry.controller.model.provider.*;
@@ -26,18 +26,18 @@ public class ProviderServiceImpl implements ProviderService {
     public List<VersionDTO> getAvailableVersions(String organization, String provider) {
         log.info("Organization Provider: {} {}", organization, provider);
         Response<List<Organization>> listOrganization = restClient.getOrganizationsByNameAndProvider(organization, provider);
-        ResponseWithInclude<List<Version>, File> versionsWithFile = restClient.getAllVersionsByProviderWithFile(listOrganization.getData().get(0).getId(), listOrganization.getData().get(0).getRelationships().getProvider().getData().get(0).getId());
+        ResponseWithInclude<List<Version>, Implementation> versionsWithFile = restClient.getAllVersionsByProviderWithImplementation(listOrganization.getData().get(0).getId(), listOrganization.getData().get(0).getRelationships().getProvider().getData().get(0).getId());
         List<VersionDTO> versionDTOList = new ArrayList<>();
         for (Version version : versionsWithFile.getData()) {
             VersionDTO versionDTO = new VersionDTO();
             versionDTO.setVersion(version.getAttributes().getVersionNumber());
             versionDTO.setProtocols(List.of(version.getAttributes().getProtocols().split(",")));
             List<PlatformDTO> platformDTOList = new ArrayList<>();
-            for (File file : versionsWithFile.getIncluded()) {
-                if (file.getRelationships().getVersion().getData().getId().equals(version.getId())) {
+            for (Implementation implementation : versionsWithFile.getIncluded()) {
+                if (implementation.getRelationships().getVersion().getData().getId().equals(version.getId())) {
                     PlatformDTO platformDTO = new PlatformDTO();
-                    platformDTO.setOs(file.getAttributes().getOs());
-                    platformDTO.setArch(file.getAttributes().getArch());
+                    platformDTO.setOs(implementation.getAttributes().getOs());
+                    platformDTO.setArch(implementation.getAttributes().getArch());
                     platformDTOList.add(platformDTO);
                 }
             }
@@ -54,25 +54,25 @@ public class ProviderServiceImpl implements ProviderService {
         String providerId = listOrganization.getData().get(0).getRelationships().getProvider().getData().get(0).getId();
         Response<List<Version>> listVersion =  restClient.getVersionsByOrganizationIdAndProviderIdAndVersionNumber(organizationId, providerId , version);
         String versionId = listVersion.getData().get(0).getId();
-        Response<List<File>> files = restClient.getFileByOsArchVersion(organizationId, providerId, versionId, os, arch);
+        Response<List<Implementation>> files = restClient.getImplementationByOsArchVersion(organizationId, providerId, versionId, os, arch);
 
         FileDTO fileDTO = new FileDTO();
-        for (File file : files.getData()) {
+        for (Implementation implementation : files.getData()) {
             fileDTO.setProtocols(List.of(listVersion.getData().get(0).getAttributes().getProtocols().split(",")));
-            fileDTO.setOs(file.getAttributes().getOs());
-            fileDTO.setArch(file.getAttributes().getArch());
-            fileDTO.setFilename(file.getAttributes().getFilename());
-            fileDTO.setDownload_url(file.getAttributes().getDownloadUrl());
-            fileDTO.setShasums_url(file.getAttributes().getShasumsUrl());
-            fileDTO.setShasums_signature_url(file.getAttributes().getShasumsSignatureUrl());
-            fileDTO.setShasum(file.getAttributes().getShasum());
+            fileDTO.setOs(implementation.getAttributes().getOs());
+            fileDTO.setArch(implementation.getAttributes().getArch());
+            fileDTO.setFilename(implementation.getAttributes().getFilename());
+            fileDTO.setDownload_url(implementation.getAttributes().getDownloadUrl());
+            fileDTO.setShasums_url(implementation.getAttributes().getShasumsUrl());
+            fileDTO.setShasums_signature_url(implementation.getAttributes().getShasumsSignatureUrl());
+            fileDTO.setShasum(implementation.getAttributes().getShasum());
 
             GpgPublicKeys gpgPublicKeys = new GpgPublicKeys();
-            gpgPublicKeys.setKey_id(file.getAttributes().getKeyId());
-            gpgPublicKeys.setAscii_armor(file.getAttributes().getAsciiArmor());
-            gpgPublicKeys.setTrust_signature(file.getAttributes().getTrustSignature());
-            gpgPublicKeys.setSource(file.getAttributes().getSource());
-            gpgPublicKeys.setSource_url(file.getAttributes().getSourceUrl());
+            gpgPublicKeys.setKey_id(implementation.getAttributes().getKeyId());
+            gpgPublicKeys.setAscii_armor(implementation.getAttributes().getAsciiArmor());
+            gpgPublicKeys.setTrust_signature(implementation.getAttributes().getTrustSignature());
+            gpgPublicKeys.setSource(implementation.getAttributes().getSource());
+            gpgPublicKeys.setSource_url(implementation.getAttributes().getSourceUrl());
 
             SigningKeys signingKeys = new SigningKeys();
             signingKeys.setGpg_public_keys(Arrays.asList(gpgPublicKeys));
@@ -80,6 +80,7 @@ public class ProviderServiceImpl implements ProviderService {
             fileDTO.setSigning_keys(signingKeys);
 
         }
+
         return fileDTO;
     }
 }
