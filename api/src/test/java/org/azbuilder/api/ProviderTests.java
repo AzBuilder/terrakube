657f6784856126eb9d27aeb525c35a0940a0e89f
Alfredo España <alfespa17@gmail.com>
2021-09-23 19:00:41 -0600

    Fix issue build

--- api/src/test/java/org/azbuilder/api/ProviderTests.java
+++ api/src/test/java/org/azbuilder/api/ProviderTests.java
@@ -52,7 +52,7 @@ public class ProviderTests extends ServerApplicationTests{
 
     @Test
     @Sql(statements = {
-            "DELETE secret; DELETE variable; DELETE environment; DELETE workspace; DELETE implementation; DELETE version; DELETE module; DELETE FROM provider; DELETE FROM team; DELETE FROM organization;",
+            "DELETE job; DELETE secret; DELETE variable; DELETE environment; DELETE workspace; DELETE implementation; DELETE version; DELETE module; DELETE FROM provider; DELETE FROM team; DELETE FROM organization;",
             "INSERT INTO organization (id, name, description) VALUES\n" +
                     "\t\t('a42f538b-8c75-4311-8e73-ea2c0f2fb577','Organization','Description');",
             "INSERT INTO team (id, name, manage_workspace, manage_module, manage_provider, organization_id) VALUES\n" +
@@ -94,7 +94,7 @@ public class ProviderTests extends ServerApplicationTests{
 
     @Test
     @Sql(statements = {
-            "DELETE secret; DELETE variable; DELETE environment; DELETE workspace; DELETE implementation; DELETE version; DELETE module; DELETE FROM provider; DELETE FROM team; DELETE FROM organization;",
+            "DELETE job; DELETE secret; DELETE variable; DELETE environment; DELETE workspace; DELETE implementation; DELETE version; DELETE module; DELETE FROM provider; DELETE FROM team; DELETE FROM organization;",
             "INSERT INTO organization (id, name, description) VALUES\n" +
                     "\t\t('a42f538b-8c75-4311-8e73-ea2c0f2fb577','Organization','Description');",
             "INSERT INTO team (id, name, manage_workspace, manage_module, manage_provider, organization_id) VALUES\n" +
