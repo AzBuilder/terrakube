9d895e1b31140b06139c7c72da781b63673f2b7c
Alfredo España <alfespa17@gmail.com>
2021-11-01 19:09:08 -0600

    Release 1.5.1

--- api-registry/src/main/java/org/azbuilder/registry/service/provider/ProviderServiceImpl.java
+++ api-registry/src/main/java/org/azbuilder/registry/service/provider/ProviderServiceImpl.java
@@ -1,7 +1,7 @@
 package org.azbuilder.registry.service.provider;
 
 import lombok.extern.slf4j.Slf4j;
-import org.azbuilder.api.client.RestClient;
+import org.azbuilder.api.client.TerrakubeClient;
 import org.azbuilder.api.client.model.organization.Organization;
 import org.azbuilder.api.client.model.organization.provider.version.Version;
 import org.azbuilder.api.client.model.organization.provider.version.implementation.Implementation;
@@ -20,18 +20,18 @@ import java.util.List;
 public class ProviderServiceImpl implements ProviderService {
 
     @Autowired
-    RestClient restClient;
+    TerrakubeClient terrakubeClient;
 
     @Override
     public List<VersionDTO> getAvailableVersions(String organization, String provider) {
         log.info("Organization Provider: {} {}", organization, provider);
-        Response<List<Organization>> listOrganization = restClient.getOrganizationsByNameAndProvider(organization, provider);
-        ResponseWithInclude<List<Version>, Implementation> versionsWithFile = restClient.getAllVersionsByProviderWithImplementation(listOrganization.getData().get(0).getId(), listOrganization.getData().get(0).getRelationships().getProvider().getData().get(0).getId());
+        Response<List<Organization>> listOrganization = terrakubeClient.getOrganizationsByNameAndProvider(organization, provider);
+        ResponseWithInclude<List<Version>, Implementation> versionsWithFile = terrakubeClient.getAllVersionsByProviderWithImplementation(listOrganization.getData().get(0).getId(), listOrganization.getData().get(0).getRelationships().getProvider().getData().get(0).getId());
         List<VersionDTO> versionDTOList = new ArrayList<>();
         for (Version version : versionsWithFile.getData()) {
             VersionDTO versionDTO = new VersionDTO();
             versionDTO.setVersion(version.getAttributes().getVersionNumber());
-            versionDTO.setProtocols(List.of(version.getAttributes().getProtocols().split(",")));
+            versionDTO.setProtocols(Arrays.asList(version.getAttributes().getProtocols().split(",")));
             List<PlatformDTO> platformDTOList = new ArrayList<>();
             for (Implementation implementation : versionsWithFile.getIncluded()) {
                 if (implementation.getRelationships().getVersion().getData().getId().equals(version.getId())) {
@@ -49,16 +49,16 @@ public class ProviderServiceImpl implements ProviderService {
 
     @Override
     public FileDTO getFileInformation(String organization, String provider, String version, String os, String arch) {
-        Response<List<Organization>> listOrganization = restClient.getOrganizationsByNameAndProvider(organization, provider);
+        Response<List<Organization>> listOrganization = terrakubeClient.getOrganizationsByNameAndProvider(organization, provider);
         String organizationId = listOrganization.getData().get(0).getId();
         String providerId = listOrganization.getData().get(0).getRelationships().getProvider().getData().get(0).getId();
-        Response<List<Version>> listVersion =  restClient.getVersionsByOrganizationIdAndProviderIdAndVersionNumber(organizationId, providerId , version);
+        Response<List<Version>> listVersion =  terrakubeClient.getVersionsByOrganizationIdAndProviderIdAndVersionNumber(organizationId, providerId , version);
         String versionId = listVersion.getData().get(0).getId();
-        Response<List<Implementation>> files = restClient.getImplementationByOsArchVersion(organizationId, providerId, versionId, os, arch);
+        Response<List<Implementation>> files = terrakubeClient.getImplementationByOsArchVersion(organizationId, providerId, versionId, os, arch);
 
         FileDTO fileDTO = new FileDTO();
         for (Implementation implementation : files.getData()) {
-            fileDTO.setProtocols(List.of(listVersion.getData().get(0).getAttributes().getProtocols().split(",")));
+            fileDTO.setProtocols(Arrays.asList(listVersion.getData().get(0).getAttributes().getProtocols().split(",")));
             fileDTO.setOs(implementation.getAttributes().getOs());
             fileDTO.setArch(implementation.getAttributes().getArch());
             fileDTO.setFilename(implementation.getAttributes().getFilename());
