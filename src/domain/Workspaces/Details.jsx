6ee55fe05712d25d067b7cccb84daa849e6b83bb
Javier Canizalez <javier-canizalez@outlook.com>
2022-01-19 18:04:34 -0600

    Implement Visual States in Workspace Page (#65)

--- src/domain/Workspaces/Details.jsx
+++ src/domain/Workspaces/Details.jsx
@@ -49,6 +49,30 @@ export const WorkspaceDetails = (props) => {
     changeJob(id);
   };
 
+  const elements = [
+    {
+      id: '1',
+      type: 'input', // input node
+      data: { label: 'Input Node' },
+      position: { x: 250, y: 25 },
+    },
+    // default node
+    {
+      id: '2',
+      // you can also pass a React component as a label
+      data: { label: <div>Default Node</div> },
+      position: { x: 100, y: 125 },
+    },
+    {
+      id: '3',
+      type: 'output', // output node
+      data: { label: 'Output Node' },
+      position: { x: 250, y: 250 },
+    },
+    // animated edge
+    { id: 'e1-2', source: '1', target: '2', animated: true },
+    { id: 'e2-3', source: '2', target: '3' },
+  ];
   const handleStatesClick =(key) => {
     switchKey(key);
   };
--- src/domain/Workspaces/States.jsx
+++ src/domain/Workspaces/States.jsx
@@ -1,69 +1,162 @@
-import { React ,useState,useRef} from 'react';
-import { List, Space,Card, Row,Col,Avatar,Button} from "antd";
+import { React, useState, useRef, useEffect } from 'react';
+import { List, Space, Card, Row, Col, Avatar, Button } from "antd";
 import Editor from "@monaco-editor/react";
-import {axiosClient} from "../../config/axiosConfig";
+import { axiosClient } from "../../config/axiosConfig";
+import ReactFlow, { Controls, Background } from 'react-flow-renderer';
+import NodeResource from './NodeResource';
 
-export const States = ({ history, setStateDetailsVisible,stateDetailsVisible  }) => {
-  const [currentState,setCurrentState] = useState({});
-  const[stateContent,setStateContent] = useState(""); 
+
+export const States = ({ history, setStateDetailsVisible, stateDetailsVisible }) => {
+  const [currentState, setCurrentState] = useState({});
+  const [stateContent, setStateContent] = useState("");
+  const [activeTab, setactivetab] = useState('diagram');
+  const [resources, setResources] = useState([]);
+
   const editorRef = useRef(null);
   const handleClick = state => {
-       changeState(state);
+    changeState(state);
   };
 
   function handleEditorDidMount(editor, monaco) {
-    editorRef.current = editor; 
+    editorRef.current = editor;
   }
 
   const changeState = state => {
     setCurrentState(state);
     setStateDetailsVisible(true);
+
     axiosClient.get(state.output).then(
       resp => {
         console.log(JSON.stringify(resp.data, null, '\t'));
         setStateContent(JSON.stringify(resp.data, null, '\t'));
+
+        let resources = [];
+        let x = new Map();
+        let y = 100;
+
+        if (resp.data != null &&  resp.data.values!= null &&resp.data.values.root_module != null) { 
+
+        resp.data.values.root_module.resources.forEach(element => {
+          let dependencies = 0;
+          if (element.depends_on != null)
+            dependencies = element.depends_on.length;
+          x.set(dependencies, (x.get(dependencies) ? x.get(dependencies) : 0) + 350);
+
+          resources.push(
+            {
+              id: element.address,
+              type: 'resourceNode',
+              data: { name: element.name, provider: element.provider_name, type: element.type },
+              position: { x: x.get(dependencies), y: (y + (dependencies * 130)) },
+            });
+
+          if (dependencies > 0) {
+            element.depends_on.forEach(dep => {
+
+              resources.push(
+                {
+                  id: element.address + "-" + dep,
+                  source: element.address,
+                  target: dep,
+                  className: 'normal-edge',
+                  animated: true,
+                  arrowHeadType: 'arrow',
+                  style: { stroke: '#1890ff' },
+                });
+            });
+          }
+        });
+
+        setResources(resources);
+       
+      }
       }
     ).catch(err => setStateContent(`{"error":"Failed to load state ${err}"}`));
   }
 
+  const tabs = [
+    {
+      key: 'diagram',
+      tab: 'diagram',
+    },
+    {
+      key: 'code',
+      tab: 'code',
+    },
+  ];
+
+  const onTabChange = key => {
+    setactivetab(key);
+  };
+
+  
+
+  const nodeTypes = {
+    resourceNode: NodeResource,
+  };
+
+
   return (
     <div>
-       {!stateDetailsVisible ? 
-      (<List split="" className="moduleList" dataSource={history}
-        renderItem={item => (
-          <List.Item>
-            <Card onClick={() => handleClick(item)} style={{ width: "100%" }} hoverable>
-              <Space className="states" size={40} split="|">
-                <span>#{item.id}</span>
-                <span><b>username</b> triggered from Terraform</span>
-                <span><a>job #32</a></span>
-                <span>{item.relativeDate}</span>
-              </Space>
-            </Card>
-          </List.Item>
-        )}/>):(
-         <div style={{paddingTop:"10px"}}>
-           <Row>
-             <Col span={1}><Avatar shape="square" src="https://avatarfiles.alphacoders.com/128/thumb-128984.png" /></Col>
-             <Col span={21}><h3>{currentState.title}</h3>
-            <Space className="stateDetails" size={40} split="|">
-                <span>#{currentState.id}</span>
-                <span><b>username</b> triggered from Terraform</span>
-                <span><a>job #32</a></span>
-              </Space>
+      {!stateDetailsVisible ?
+        (<List split="" className="moduleList" dataSource={history}
+          renderItem={item => (
+            <List.Item>
+              <Card onClick={() => handleClick(item)} style={{ width: "100%" }} hoverable>
+                <Space className="states" size={40} split="|">
+                  <span>#{item.id}</span>
+                  <span><b>username</b> triggered from Terraform</span>
+                  <span><a>job #32</a></span>
+                  <span>{item.relativeDate}</span>
+                </Space>
+              </Card>
+            </List.Item>
+          )} />) : (
+          <div style={{ paddingTop: "10px" }}>
+            <Row>
+              <Col span={1}><Avatar shape="square" src="https://avatarfiles.alphacoders.com/128/thumb-128984.png" /></Col>
+              <Col span={21}><h3>{currentState.title}</h3>
+                <Space className="stateDetails" size={40} split="|">
+                  <span>#{currentState.id}</span>
+                  <span><b>username</b> triggered from Terraform</span>
+                  <span><a>job #32</a></span>
+                </Space>
               </Col>
               <Col span={2}>
-                  <Button target="_blank" href={currentState.output}>Download</Button><br/>
-                  <span className="stateDetails">{currentState.relativeDate}</span>
+                <Button target="_blank" href={currentState.output}>Download</Button><br />
+                <span className="stateDetails">{currentState.relativeDate}</span>
               </Col>
             </Row>
-            <Row style={{paddingTop:"30px"}}>
-            <Editor height="60vh" options={{readOnly:"true"}} onMount={handleEditorDidMount} defaultLanguage="json" defaultValue={stateContent}/>
+            <Row style={{ paddingTop: "30px" }}>
+              <Col span={24}>
+
+                <Card
+                  style={{ width: '100%' }}
+                  tabList={tabs}
+                  activeTabKey={activeTab}
+                  onTabChange={key => {
+                    onTabChange(key);
+                  }}
+                >
+                  {activeTab === "diagram" ? (
+                    <div style={{ height: 500 }}>
+                      <ReactFlow zoomOnScroll={false} nodeTypes={nodeTypes}  elements={resources}>
+                        <Controls />
+                        <Background />
+                      </ReactFlow>
+                    </div>
+
+                  ) : (
+                    <Editor height="60vh" options={{ readOnly: "true" }} onMount={handleEditorDidMount} defaultLanguage="json" defaultValue={stateContent} />
+                  )}
+
+
+                </Card>
+
+              </Col>
             </Row>
-            <Row style={{paddingTop:"30px"}}>
-              <h3>Changes in this version</h3>
-            </Row>
-         </div>
+
+          </div>
         )}
     </div>
   )
