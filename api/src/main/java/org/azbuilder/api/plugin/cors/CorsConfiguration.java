dca1202d8393af70ec7aee290ddc8ae1838af84b
Alfredo España <alfespa17@gmail.com>
2022-04-23 09:06:07 -0600

    Fix cors issue with UI

---
+++ api/src/main/java/org/azbuilder/api/plugin/cors/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package org.azbuilder.api.plugin.cors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Value( "${jdbc.url:http://localhost:3000}" )
+    private String uiURL;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(uiURL);
+            }
+        };
+    }
+}
