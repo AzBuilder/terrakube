69a50800a293f995b7b11004481dac42f3abb260
Alfredo España <alfespa17@gmail.com>
2022-04-25 17:14:08 -0600

    Aws S3 initial support

---
+++ api-registry/src/main/java/org/azbuilder/registry/plugin/storage/aws/AwsStorageServiceProperties.java
@@ -0,0 +1,24 @@
+package org.azbuilder.registry.plugin.storage.aws;
+
+import com.amazonaws.regions.Regions;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@PropertySources({
+        @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true),
+        @PropertySource(value = "classpath:application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
+})
+@ConfigurationProperties(prefix = "org.terrakube.registry.plugin.storage.aws")
+public class AwsStorageServiceProperties {
+    private String accessKey;
+    private String secretKey;
+    private String bucketName;
+    private Regions region;
+}
--- api-registry/src/main/java/org/azbuilder/registry/plugin/storage/configuration/StorageAutoConfiguration.java
+++ api-registry/src/main/java/org/azbuilder/registry/plugin/storage/configuration/StorageAutoConfiguration.java
@@ -1,8 +1,15 @@
 package org.azbuilder.registry.plugin.storage.configuration;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import org.azbuilder.registry.plugin.storage.StorageService;
+import org.azbuilder.registry.plugin.storage.aws.AwsStorageServiceImpl;
+import org.azbuilder.registry.plugin.storage.aws.AwsStorageServiceProperties;
 import org.azbuilder.registry.plugin.storage.azure.AzureStorageServiceImpl;
 import org.azbuilder.registry.plugin.storage.azure.AzureStorageServiceProperties;
 import org.azbuilder.registry.plugin.storage.local.LocalStorageServiceImpl;
@@ -21,7 +28,7 @@ import org.springframework.context.annotation.Configuration;
 public class StorageAutoConfiguration {
 
     @Bean
-    public StorageService terraformOutput(StorageProperties storageProperties, AzureStorageServiceProperties azureStorageServiceProperties) {
+    public StorageService terraformOutput(StorageProperties storageProperties, AzureStorageServiceProperties azureStorageServiceProperties, AwsStorageServiceProperties awsStorageServiceProperties) {
         StorageService storageService = null;
         if (storageProperties != null)
             switch (storageProperties.getType()) {
@@ -36,6 +43,25 @@ public class StorageAutoConfiguration {
                     storageService = AzureStorageServiceImpl.builder()
                             .blobServiceClient(blobServiceClient)
                             .gitService(new GitServiceImpl())
+                            .build();
+                    break;
+                case AwsStorageImpl:
+
+                    AWSCredentials credentials = new BasicAWSCredentials(
+                            awsStorageServiceProperties.getAccessKey(),
+                            awsStorageServiceProperties.getSecretKey()
+                    );
+
+                    AmazonS3 s3client = AmazonS3ClientBuilder
+                            .standard()
+                            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                            .withRegion(awsStorageServiceProperties.getRegion())
+                            .build();
+
+                    storageService = AwsStorageServiceImpl.builder()
+                            .s3client(s3client)
+                            .gitService(new GitServiceImpl())
+                            .bucketName(awsStorageServiceProperties.getBucketName())
                             .build();
                     break;
                 case Local:
