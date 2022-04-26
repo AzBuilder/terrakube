a4367b128ec43be8b6627454b25d9687b8817076
Alfredo España <alfespa17@gmail.com>
2022-04-25 20:11:16 -0600

    Fix cors v3 (#160)
    * Fix cors v3

--- api/src/main/java/org/azbuilder/api/plugin/security/authentication/azure/AADOAuth2ResourceServerSecurityConfig.java
+++ api/src/main/java/org/azbuilder/api/plugin/security/authentication/azure/AADOAuth2ResourceServerSecurityConfig.java
@@ -1,28 +1,48 @@
 package org.azbuilder.api.plugin.security.authentication.azure;
 
 import com.azure.spring.aad.webapi.AADResourceServerWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @ConditionalOnProperty(prefix = "org.azbuilder.api.authentication", name = "type", havingValue = "AZURE")
 public class AADOAuth2ResourceServerSecurityConfig extends AADResourceServerWebSecurityConfigurerAdapter {
-    /**
-     * Add configuration logic as needed.
-     */
+
+    @Value( "${org.terrakube.ui.url:http://localhost:3000}" )
+    private String uiURL;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         //http.authorizeRequests(requests -> requests.anyRequest().authenticated());
-        http.authorizeRequests()
+        http.cors().and().authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                 .antMatchers("/callback/v1/**").permitAll()
                 .antMatchers("/doc").permitAll()
                 .and()
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(uiURL));
+        configuration.setAllowedMethods(Arrays.asList("PATCH","GET","HEAD","DELETE","POST","OPTIONS", "PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
