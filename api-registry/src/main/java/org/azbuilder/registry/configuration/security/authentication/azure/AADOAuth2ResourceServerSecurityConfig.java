3122e14f5176cdbd1013f77eaf01a676c3235471
Alfredo España <alfespa17@gmail.com>
2022-04-26 17:52:16 -0600

    Fix cors again

--- api-registry/src/main/java/org/azbuilder/registry/configuration/security/authentication/azure/AADOAuth2ResourceServerSecurityConfig.java
+++ api-registry/src/main/java/org/azbuilder/registry/configuration/security/authentication/azure/AADOAuth2ResourceServerSecurityConfig.java
@@ -37,8 +37,10 @@ public class AADOAuth2ResourceServerSecurityConfig extends AADResourceServerWebS
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(uiURL));
-        configuration.setAllowedMethods(Arrays.asList("PATCH","GET","HEAD","DELETE","POST","OPTIONS", "PUT"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
--- api/src/main/java/org/azbuilder/api/plugin/security/authentication/azure/AADOAuth2ResourceServerSecurityConfig.java
+++ api/src/main/java/org/azbuilder/api/plugin/security/authentication/azure/AADOAuth2ResourceServerSecurityConfig.java
@@ -1,6 +1,7 @@
 package org.azbuilder.api.plugin.security.authentication.azure;
 
 import com.azure.spring.aad.webapi.AADResourceServerWebSecurityConfigurerAdapter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ import java.util.Arrays;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @ConditionalOnProperty(prefix = "org.azbuilder.api.authentication", name = "type", havingValue = "AZURE")
+@Slf4j
 public class AADOAuth2ResourceServerSecurityConfig extends AADResourceServerWebSecurityConfigurerAdapter {
 
     @Value( "${org.terrakube.ui.url:http://localhost:3000}" )
@@ -38,9 +40,12 @@ public class AADOAuth2ResourceServerSecurityConfig extends AADResourceServerWebS
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
+        log.info("Loading CORS {}", uiURL);
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(uiURL));
-        configuration.setAllowedMethods(Arrays.asList("PATCH","GET","HEAD","DELETE","POST","OPTIONS", "PUT"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin", "Cache-Control", "Content-Type", "Authorization"));
+        configuration.setAllowedMethods(Arrays.asList("DELETE", "GET", "POST", "PATCH", "PUT", "OPTIONS"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
