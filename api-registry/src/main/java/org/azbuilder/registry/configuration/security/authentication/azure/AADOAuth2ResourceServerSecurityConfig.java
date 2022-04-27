d355c32f81366bef61487664bddbcc2a92c213d9
Alfredo España <alfespa17@gmail.com>
2022-04-27 17:24:30 -0600

    Add suppor for multiple domains in CORS

--- api-registry/src/main/java/org/azbuilder/registry/configuration/security/authentication/azure/AADOAuth2ResourceServerSecurityConfig.java
+++ api-registry/src/main/java/org/azbuilder/registry/configuration/security/authentication/azure/AADOAuth2ResourceServerSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -39,7 +40,7 @@ public class AADOAuth2ResourceServerSecurityConfig extends AADResourceServerWebS
     CorsConfigurationSource corsConfigurationSource() {
         log.info("CORS for UI Domain {}", uiDomain);
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(uiDomain));
+        configuration.setAllowedOrigins(List.of(uiDomain.split(",")));
         configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin", "Cache-Control", "Content-Type", "Authorization"));
         configuration.setAllowedMethods(Arrays.asList("DELETE", "GET", "POST", "PATCH", "PUT", "OPTIONS"));
         configuration.setAllowCredentials(true);
