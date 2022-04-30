70bb03450b5e0f36a516e9ea9d365f21cfa905ae
Alfredo España <alfespa17@gmail.com>
2022-04-29 18:53:19 -0600

    Small logic fix

--- api-registry/src/main/java/org/azbuilder/registry/plugin/storage/configuration/StorageAutoConfiguration.java
+++ api-registry/src/main/java/org/azbuilder/registry/plugin/storage/configuration/StorageAutoConfiguration.java
@@ -34,48 +34,45 @@ public class StorageAutoConfiguration {
     public StorageService terraformOutput(StorageProperties storageProperties, AzureStorageServiceProperties azureStorageServiceProperties, AwsStorageServiceProperties awsStorageServiceProperties) {
         StorageService storageService = null;
         log.info("StorageType=", storageProperties.getType());
-        if (storageProperties != null)
-            switch (storageProperties.getType()) {
-                case AzureStorageImpl:
-                    BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-                            .connectionString(
-                                    String.format("DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;EndpointSuffix=core.windows.net",
-                                            azureStorageServiceProperties.getAccountName(),
-                                            azureStorageServiceProperties.getAccountKey())
-                            ).buildClient();
+        switch (storageProperties.getType()) {
+            case AzureStorageImpl:
+                BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                        .connectionString(
+                                String.format("DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;EndpointSuffix=core.windows.net",
+                                        azureStorageServiceProperties.getAccountName(),
+                                        azureStorageServiceProperties.getAccountKey())
+                        ).buildClient();
 
-                    storageService = AzureStorageServiceImpl.builder()
-                            .blobServiceClient(blobServiceClient)
-                            .gitService(new GitServiceImpl())
-                            .build();
-                    break;
-                case AwsStorageImpl:
+                storageService = AzureStorageServiceImpl.builder()
+                        .blobServiceClient(blobServiceClient)
+                        .gitService(new GitServiceImpl())
+                        .build();
+                break;
+            case AwsStorageImpl:
 
-                    AWSCredentials credentials = new BasicAWSCredentials(
-                            awsStorageServiceProperties.getAccessKey(),
-                            awsStorageServiceProperties.getSecretKey()
-                    );
+                AWSCredentials credentials = new BasicAWSCredentials(
+                        awsStorageServiceProperties.getAccessKey(),
+                        awsStorageServiceProperties.getSecretKey()
+                );
 
-                    AmazonS3 s3client = AmazonS3ClientBuilder
-                            .standard()
-                            .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                            .withRegion(Regions.fromName(awsStorageServiceProperties.getRegion()))
-                            .build();
+                AmazonS3 s3client = AmazonS3ClientBuilder
+                        .standard()
+                        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                        .withRegion(Regions.fromName(awsStorageServiceProperties.getRegion()))
+                        .build();
 
-                    storageService = AwsStorageServiceImpl.builder()
-                            .s3client(s3client)
-                            .gitService(new GitServiceImpl())
-                            .bucketName(awsStorageServiceProperties.getBucketName())
-                            .build();
-                    break;
-                case Local:
-                    storageService = new LocalStorageServiceImpl();
-                    break;
-                default:
-                    storageService = null;
-            }
-        else
-            storageService = null;
+                storageService = AwsStorageServiceImpl.builder()
+                        .s3client(s3client)
+                        .gitService(new GitServiceImpl())
+                        .bucketName(awsStorageServiceProperties.getBucketName())
+                        .build();
+                break;
+            case Local:
+                storageService = new LocalStorageServiceImpl();
+                break;
+            default:
+                storageService = null;
+        }
         return storageService;
     }
 }
