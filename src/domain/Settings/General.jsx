e0d0a7731c53ca02b9288d5617a0fc66a299f102
Javier Canizalez <javier-canizalez@outlook.com>
2022-05-02 18:15:50 -0600

    Support Edit in organization settings (#91)

--- src/domain/Settings/General.jsx
+++ src/domain/Settings/General.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from "react";
-import { Button, Form, Input} from "antd";
+import { Button, Form,Input,message,Spin} from "antd";
 import axiosInstance from "../../config/axiosConfig";
 import {useParams} from "react-router-dom";
 import './Settings.css';
@@ -8,6 +8,7 @@ export const GeneralSettings = () => {
   const { orgid } = useParams();
   const [organization, setOrganization] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [waiting, setWaiting] = useState(false);
   useEffect(() => {
     setLoading(true);
     axiosInstance.get(`organization/${orgid}`)
@@ -18,13 +19,48 @@ export const GeneralSettings = () => {
       });
 
   }, [orgid]);
+
+  const onFinish = (values) => {
+    setWaiting(true);
+    const body = {
+      data: {
+        type: "organization",
+        id: orgid,
+        attributes: {
+          name: values.name,
+          description: values.description,
+        }
+      }
+    }
+    console.log(body);
+
+    axiosInstance.patch(`organization/${orgid}`, body, {
+      headers: {
+        'Content-Type': 'application/vnd.api+json'
+      }
+    })
+      .then(response => {
+        console.log(response);
+        if (response.status == "204") {
+          message.success('Organization updated successfully');
+
+        }
+        else{
+          message.error('Organization update failed');
+        }
+        setWaiting(false);
+
+      })
+  };
+  
   return (
     <div className="setting" >
       <h1>General Settings</h1>
       {loading || !organization.data ? (
           <p>Data loading...</p>
         ) : (
-      <Form layout="vertical" name="form-settings" >
+          <Spin spinning={waiting}>
+      <Form onFinish={onFinish} layout="vertical" name="form-settings" >
         <Form.Item name="name" label="Name" >
           <Input defaultValue={organization.data.attributes.name} />
         </Form.Item>
@@ -36,7 +72,7 @@ export const GeneralSettings = () => {
             Update organization
           </Button>
         </Form.Item>
-      </Form>)}
+      </Form></Spin>)}
       <h1>Delete this Organization</h1>
       <div className="App-text">
         Deleting the devops-mindset organization will permanently delete all workspaces associated with it. Please be certain that you understand this. This action cannot be undone.
