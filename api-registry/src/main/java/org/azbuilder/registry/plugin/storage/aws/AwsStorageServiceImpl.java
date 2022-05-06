3d3751bac0e0f3d49593268d593919706228d8fc
Alfredo España <alfespa17@gmail.com>
2022-05-06 11:31:04 -0600

    AWS S3 Support (#169)
    * Aws S3 initial support

---
+++ api-registry/src/main/java/org/azbuilder/registry/plugin/storage/aws/AwsStorageServiceImpl.java
@@ -0,0 +1,58 @@
+package org.azbuilder.registry.plugin.storage.aws;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.azbuilder.registry.plugin.storage.StorageService;
+import org.azbuilder.registry.service.git.GitService;
+import org.zeroturnaround.zip.ZipUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
+@Builder
+public class AwsStorageServiceImpl implements StorageService {
+
+    @NonNull
+    private AmazonS3 s3client;
+
+    @NonNull
+    private String bucketName;
+
+    @NonNull
+    GitService gitService;
+
+    @Override
+    public String searchModule(String organizationName, String moduleName, String providerName, String moduleVersion, String source, String vcsType, String accessToken) {
+        String blobKey = String.format("registry/%s/%s/%s/%s/module.zip", organizationName, moduleName, providerName, moduleVersion);
+        log.info("Checking Aws S3 Object exist {}", blobKey);
+
+        if (!s3client.doesObjectExist(bucketName, blobKey)) {
+            File gitCloneDirectory = gitService.getCloneRepositoryByTag(source, moduleVersion, vcsType, accessToken);
+            File moduleZip = new File(gitCloneDirectory.getAbsolutePath() + ".zip");
+            ZipUtil.pack(gitCloneDirectory, moduleZip);
+
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, blobKey, moduleZip);
+            AccessControlList acl = new AccessControlList();
+            acl.grantPermission(GroupGrantee.AllUsers, Permission.Read);
+            putObjectRequest.setAccessControlList(acl);
+
+            s3client.putObject(putObjectRequest);
+
+            log.info("Upload Aws S3 Object completed", blobKey);
+            try {
+                FileUtils.cleanDirectory(gitCloneDirectory);
+                if (moduleZip.delete()) log.info("Successfully delete folder");
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+        }
+
+        return s3client.getUrl(bucketName, blobKey).toExternalForm();
+    }
+
+}
--- api-registry/src/main/java/org/azbuilder/registry/plugin/storage/configuration/StorageAutoConfiguration.java
+++ api-registry/src/main/java/org/azbuilder/registry/plugin/storage/configuration/StorageAutoConfiguration.java
@@ -1,8 +1,17 @@
 package org.azbuilder.registry.plugin.storage.configuration;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.azbuilder.registry.plugin.storage.StorageService;
+import org.azbuilder.registry.plugin.storage.aws.AwsStorageServiceImpl;
+import org.azbuilder.registry.plugin.storage.aws.AwsStorageServiceProperties;
 import org.azbuilder.registry.plugin.storage.azure.AzureStorageServiceImpl;
 import org.azbuilder.registry.plugin.storage.azure.AzureStorageServiceProperties;
 import org.azbuilder.registry.plugin.storage.local.LocalStorageServiceImpl;
@@ -18,34 +27,52 @@ import org.springframework.context.annotation.Configuration;
         StorageProperties.class
 })
 @ConditionalOnMissingBean(StorageService.class)
+@Slf4j
 public class StorageAutoConfiguration {
 
     @Bean
-    public StorageService terraformOutput(StorageProperties storageProperties, AzureStorageServiceProperties azureStorageServiceProperties) {
+    public StorageService terraformOutput(StorageProperties storageProperties, AzureStorageServiceProperties azureStorageServiceProperties, AwsStorageServiceProperties awsStorageServiceProperties) {
         StorageService storageService = null;
-        if (storageProperties != null)
-            switch (storageProperties.getType()) {
-                case AzureStorageImpl:
-                    BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-                            .connectionString(
-                                    String.format("DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;EndpointSuffix=core.windows.net",
-                                            azureStorageServiceProperties.getAccountName(),
-                                            azureStorageServiceProperties.getAccountKey())
-                            ).buildClient();
+        log.info("StorageType=", storageProperties.getType());
+        switch (storageProperties.getType()) {
+            case AzureStorageImpl:
+                BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                        .connectionString(
+                                String.format("DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;EndpointSuffix=core.windows.net",
+                                        azureStorageServiceProperties.getAccountName(),
+                                        azureStorageServiceProperties.getAccountKey())
+                        ).buildClient();
 
-                    storageService = AzureStorageServiceImpl.builder()
-                            .blobServiceClient(blobServiceClient)
-                            .gitService(new GitServiceImpl())
-                            .build();
-                    break;
-                case Local:
-                    storageService = new LocalStorageServiceImpl();
-                    break;
-                default:
-                    storageService = null;
-            }
-        else
-            storageService = null;
+                storageService = AzureStorageServiceImpl.builder()
+                        .blobServiceClient(blobServiceClient)
+                        .gitService(new GitServiceImpl())
+                        .build();
+                break;
+            case AwsStorageImpl:
+
+                AWSCredentials credentials = new BasicAWSCredentials(
+                        awsStorageServiceProperties.getAccessKey(),
+                        awsStorageServiceProperties.getSecretKey()
+                );
+
+                AmazonS3 s3client = AmazonS3ClientBuilder
+                        .standard()
+                        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                        .withRegion(Regions.fromName(awsStorageServiceProperties.getRegion()))
+                        .build();
+
+                storageService = AwsStorageServiceImpl.builder()
+                        .s3client(s3client)
+                        .gitService(new GitServiceImpl())
+                        .bucketName(awsStorageServiceProperties.getBucketName())
+                        .build();
+                break;
+            case Local:
+                storageService = new LocalStorageServiceImpl();
+                break;
+            default:
+                storageService = null;
+        }
         return storageService;
     }
 }
