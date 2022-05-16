c316d1a9f6866c0316a3b8245ce44060abd778fe
Alfredo España <alfespa17@gmail.com>
2022-05-16 17:09:47 -0600

    Refactor

--- api/src/main/java/org/azbuilder/api/plugin/storage/aws/AwsStorageTypeProperties.java
+++ api/src/main/java/org/azbuilder/api/plugin/storage/aws/AwsStorageTypeProperties.java
@@ -4,16 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
 @Setter
-@PropertySources({
-        @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true),
-        @PropertySource(value = "classpath:application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
-})
+@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
+@PropertySource(value = "classpath:application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
 @ConfigurationProperties(prefix = "org.terrakube.storage.aws")
 public class AwsStorageTypeProperties {
     private String accessKey;
--- api/src/main/java/org/azbuilder/api/plugin/storage/gcp/GcpStorageTypeProperties.java
+++ api/src/main/java/org/azbuilder/api/plugin/storage/gcp/GcpStorageTypeProperties.java
@@ -1,4 +1,20 @@
 package org.azbuilder.api.plugin.storage.gcp;
 
-public class GcpStorageTypeProperties {
+import org.azbuilder.api.plugin.storage.StorageTypeService;
+
+public class GcpStorageTypeProperties implements StorageTypeService {
+    @Override
+    public byte[] getStepOutput(String organizationId, String jobId, String stepId) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] getTerraformPlan(String organizationId, String workspaceId, String jobId, String stepId) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] getTerraformStateJson(String organizationId, String workspaceId, String stateFileName) {
+        return new byte[0];
+    }
 }
