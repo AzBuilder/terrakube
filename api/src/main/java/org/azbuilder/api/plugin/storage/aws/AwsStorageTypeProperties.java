c3ff9bac23bfc3d16bc0a3a3f460dfdbd9b060d8
Alfredo España <alfespa17@gmail.com>
2022-05-16 17:16:46 -0600

    Adding new endpoints for output and state (#175)
    * New endpoint to expose logs and state from Azure storage account and AWS S3

---
+++ api/src/main/java/org/azbuilder/api/plugin/storage/aws/AwsStorageTypeProperties.java
@@ -0,0 +1,20 @@
+package org.azbuilder.api.plugin.storage.aws;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
+@PropertySource(value = "classpath:application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
+@ConfigurationProperties(prefix = "org.terrakube.storage.aws")
+public class AwsStorageTypeProperties {
+    private String accessKey;
+    private String secretKey;
+    private String bucketName;
+    private String region;
+}
