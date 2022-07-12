a3d23568afa924a8ae0425deb12f7dcaad79fd3f
Alfredo España <alfespa17@gmail.com>
2022-07-11 18:02:06 -0600

    Adding support to handle PAT inside the registry and the internal key

--- registry/src/main/java/org/terrakube/registry/configuration/authentication/dex/DexWebSecurityAdapter.java
+++ registry/src/main/java/org/terrakube/registry/configuration/authentication/dex/DexWebSecurityAdapter.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,8 +24,17 @@ import java.util.List;
 @ConditionalOnProperty(prefix = "org.terrakube.registry.authentication", name = "type", havingValue = "DEX")
 public class DexWebSecurityAdapter extends WebSecurityConfigurerAdapter {
 
-    @Value( "${org.terrakube.ui.fqdn:http://localhost:3000}" )
+    @Value("${org.terrakube.ui.fqdn:http://localhost:3000}")
     private String uiDomain;
+
+    @Value("${org.terrakube.token.issuer-uri}")
+    private String issuerUri;
+
+    @Value("${org.terrakube.token.pat}")
+    private String patJwtSecret;
+
+    @Value("${org.terrakube.token.internal}")
+    private String internalJwtSecret;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,9 +42,17 @@ public class DexWebSecurityAdapter extends WebSecurityConfigurerAdapter {
                         .antMatchers("/.well-known/**").permitAll()
                         .antMatchers("/actuator/**").permitAll()
                         .antMatchers("/terraform/modules/v1/download/**").permitAll()
-                        .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+                .oauth2ResourceServer(oauth2 -> {
+                    AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver = DexAuthenticationManagerResolver
+                            .builder()
+                            .dexIssuerUri(this.issuerUri)
+                            .patJwtSecret(this.patJwtSecret)
+                            .internalJwtSecret(this.internalJwtSecret)
+                            .build();
+                    oauth2.authenticationManagerResolver(authenticationManagerResolver);
+                });
     }
 
     @Bean
