ccab55a4787e89feee54bb1c367366e234aafb55
Alfredo España <alfespa17@gmail.com>
2022-07-19 23:22:56 +0000

    refactor logic

---
+++ .gitpod/scripts/loadDex.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp .gitpod/template/dex/template-config-ldap.yaml .gitpod/setup/dex/config-ldap.yaml
+jwtIssuer=$(gp url 5556)
+sed -i "s+TEMPLATE_GITPOD_JWT_ISSUER+$jwtIssuer+gi" .gitpod/setup/dex/config-ldap.yaml
+docker-compose -f .gitpod/setup/dex/docker-compose.yaml up -d
+clear
+exit
