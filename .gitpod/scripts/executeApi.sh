182d815029edc300111840d7d0ee5b6a3e0b8ab2
Alfredo España <alfespa17@gmail.com>
2022-07-20 00:43:03 +0000

    Add api and regsitry startup logic

---
+++ .gitpod/scripts/executeApi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export ApiDataSourceType="H2"
+export GroupValidationType="DEX"
+export UserValidationType="DEX"
+export AuthenticationValidationType="DEX"
+export TerrakubeHostname=$(gp url 8080 | sed "s+https://++g")
+export AzBuilderExecutorUrl="$(gp url 5556)/api/v1/terraform-rs"
+export PatSecret=ejZRSFgheUBOZXAyUURUITUzdmdINDNeUGpSWHlDM1g=
+export InternalSecret=S2JeOGNNZXJQTlpWNmhTITkha2NEKkt1VVBVQmFeQjM=
+export DexIssuerUri="$(gp url 5556)/dex"
+export StorageType="LOCAL"
+export TerrakubeUiURL=$(gp url 3000)
+
+cd api
+mvn spring-boot:run
+
---
+++ .gitpod/scripts/executeRegistry.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export AzBuilderRegistry=$(gp url 8075)
+export AzBuilderApiUrl=$(gp url 8080)
+export AuthenticationValidationTypeRegistry=DEX
+export TerrakubeEnableSecurity=true
+export DexIssuerUri="$(gp url 5556)/dex"
+export TerrakubeUiURL=$(gp url 3000)
+export PatSecret=ejZRSFgheUBOZXAyUURUITUzdmdINDNeUGpSWHlDM1g=
+export InternalSecret=S2JeOGNNZXJQTlpWNmhTITkha2NEKkt1VVBVQmFeQjM=
+export RegistryStorageType=Local
+export AppClientId=example-app
+export AppIssuerUri="$(gp url 5556)/dex"
+
+cd registry
+mvn spring-boot:run
+
