855a8945765cb10b9c554850053516d0e81d1148
Alfredo España <alfespa17@gmail.com>
2022-07-21 20:46:44 +0000

    automatic ui setup

--- .gitpod/scripts/loadDex.sh
+++ .gitpod/scripts/loadDex.sh
@@ -3,6 +3,8 @@
 cp .gitpod/template/dex/template-config-ldap.yaml .gitpod/setup/dex/config-ldap.yaml
 jwtIssuer=$(gp url 5556)
 sed -i "s+TEMPLATE_GITPOD_JWT_ISSUER+$jwtIssuer+gi" .gitpod/setup/dex/config-ldap.yaml
+uiRedirect=$(gp url 3000)
+sed -i "s+TEMPLATE_GITPOD_REDIRECT+$uiRedirect+gi" .gitpod/setup/dex/config-ldap.yaml
 docker-compose -f .gitpod/setup/dex/docker-compose.yaml up -d
 clear
 echo "Dex Enviroment setup completed, you can close this terminal"
