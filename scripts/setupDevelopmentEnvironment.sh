9f73484a1976b712f0722ddf90d61953e7729de0
Alfredo España <alfespa17@gmail.com>
2022-07-23 11:03:54 -0600

    refactor scripts

---
+++ scripts/setupDevelopmentEnvironment.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+
+function generateApiVars(){
+  USER=$(whoami)
+  if [ "$USER" = "gitpod" ]; then
+    TerrakubeHostname=$(gp url 8080 | sed "s+https://++g")
+    AzBuilderExecutorUrl="$(gp url 8090)/api/v1/terraform-rs"
+    DexIssuerUri="$(gp url 5556)/dex"
+    TerrakubeUiURL=$(gp url 3000)
+  else
+    TerrakubeHostname="http://localhost:8080"
+    AzBuilderExecutorUrl="http://localhost:8090/api/v1/terraform-rs"
+    DexIssuerUri="http://localhost:5556/dex"
+    TerrakubeUiURL="http://localhost:3000"
+  fi
+
+  ApiDataSourceType="H2"
+  GroupValidationType="DEX"
+  UserValidationType="DEX"
+  AuthenticationValidationType="DEX"
+  PatSecret=ejZRSFgheUBOZXAyUURUITUzdmdINDNeUGpSWHlDM1g=
+  InternalSecret=S2JeOGNNZXJQTlpWNmhTITkha2NEKkt1VVBVQmFeQjM=
+
+  StorageType="LOCAL"
+
+  JAVA_TOOL_OPTIONS="-Xmx512m -Xms256m"
+
+  rm -f .envApi
+
+  echo "ApiDataSourceType=$ApiDataSourceType" >> .envApi
+  echo "GroupValidationType=$GroupValidationType" >> .envApi
+  echo "UserValidationType=$UserValidationType" >> .envApi
+  echo "AuthenticationValidationType=$AuthenticationValidationType" >> .envApi
+  echo "TerrakubeHostname=$TerrakubeHostname" >> .envApi
+  echo "AzBuilderExecutorUrl=$AzBuilderExecutorUrl" >> .envApi 
+  echo "PatSecret=$PatSecret" >> .envApi
+  echo "InternalSecret=$InternalSecret" >> .envApi
+  echo "DexIssuerUri=$DexIssuerUri" >> .envApi
+  echo "StorageType=$StorageType" >> .envApi
+  echo "TerrakubeUiURL=$TerrakubeUiURL" >> .envApi
+  echo "spring_profiles_active=demo" >> .envApi
+}
+
+function generateRegistryVars(){
+  USER=$(whoami)
+  if [ "$USER" = "gitpod" ]; then
+    AzBuilderApiUrl=$(gp url 8080)
+    TerrakubeRegistryDomain=$(gp url 8075)
+    TerrakubeApiUrl=$(gp url 8080)
+  else
+    AzBuilderApiUrl="http://localhost:8080"
+    TerrakubeRegistryDomain="http://localhost:8075"
+    TerrakubeApiUrl="htp://localhost:8080"
+  fi
+
+  TerrakubeEnableSecurity=true
+  InternalSecret=S2JeOGNNZXJQTlpWNmhTITkha2NEKkt1VVBVQmFeQjM=
+  TerraformStateType=LocalTerraformStateImpl
+  TerraformOutputType=LocalTerraformOutputImpl
+  ExecutorFlagBatch=false
+  ExecutorFlagDisableAcknowledge=false
+  TerrakubeToolsRepository=https://github.com/AzBuilder/terrakube-extensions.git
+  TerrakubeToolsBranch=main
+  
+  JAVA_TOOL_OPTIONS="-Xmx512m -Xms256m"
+
+  rm -f  .envExecutor
+
+  echo "TerrakubeEnableSecurity=$TerrakubeEnableSecurity" >> .envExecutor
+  echo "InternalSecret=$InternalSecret" >> .envExecutor
+  echo "TerraformStateType=$TerraformStateType" >> .envExecutor
+  echo "TerraformOutputType=$TerraformOutputType" >> .envExecutor
+  echo "AzBuilderApiUrl=$AzBuilderApiUrl" >> .envExecutor
+  echo "ExecutorFlagBatch=$ExecutorFlagBatch" >> .envExecutor
+  echo "ExecutorFlagDisableAcknowledge=$ExecutorFlagDisableAcknowledge" >> .envExecutor
+  echo "TerrakubeToolsRepository=$TerrakubeToolsRepository" >> .envExecutor
+  echo "TerrakubeToolsBranch=$TerrakubeToolsBranch" >> .envExecutor
+  echo "TerrakubeRegistryDomain=$TerrakubeRegistryDomain" >> .envExecutor
+  echo "TerrakubeApiUrl=$TerrakubeApiUrl" >> .envExecutor
+  echo "JAVA_TOOL_OPTIONS=$JAVA_TOOL_OPTIONS" >> .envExecutor
+}
+
+function generateExecutorVars(){
+  USER=$(whoami)
+  if [ "$USER" = "gitpod" ]; then
+    AzBuilderRegistry=$(gp url 8075)
+    AzBuilderApiUrl=$(gp url 8080)
+    DexIssuerUri="$(gp url 5556)/dex"
+    TerrakubeUiURL=$(gp url 3000)
+    AppIssuerUri="$(gp url 5556)/dex"
+  else
+    AzBuilderRegistry="http://localhost:8075"
+    AzBuilderApiUrl="http://localhost:8080"
+    DexIssuerUri="http://localhost:5556/dex"
+    TerrakubeUiURL="http://localhost:3000"
+    AppIssuerUri="http://localhost:5556/dex"
+  fi
+
+  AuthenticationValidationTypeRegistry=DEX
+  TerrakubeEnableSecurity=true
+  PatSecret=ejZRSFgheUBOZXAyUURUITUzdmdINDNeUGpSWHlDM1g=
+  InternalSecret=S2JeOGNNZXJQTlpWNmhTITkha2NEKkt1VVBVQmFeQjM=
+  RegistryStorageType=Local
+  AppClientId=example-app
+
+  JAVA_TOOL_OPTIONS="-Xmx256m -Xms128m"
+
+  rm -f .envRegistry
+
+  echo "AzBuilderRegistry=$AzBuilderRegistry" >> .envRegistry
+  echo "AzBuilderApiUrl=$AzBuilderApiUrl" >> .envRegistry
+  echo "AuthenticationValidationTypeRegistry=$AuthenticationValidationTypeRegistry" >> .envRegistry
+  echo "TerrakubeEnableSecurity=$TerrakubeEnableSecurity" >> .envRegistry
+  echo "DexIssuerUri=$DexIssuerUri" >> .envRegistry
+  echo "TerrakubeUiURL=$TerrakubeUiURL" >> .envRegistry
+  echo "PatSecret=$PatSecret" >> .envRegistry
+  echo "InternalSecret=$InternalSecret" >> .envRegistry
+  echo "RegistryStorageType=$RegistryStorageType" >> .envRegistry
+  echo "AppClientId=$AppClientId" >> .envRegistry
+  echo "AppIssuerUri=$AppIssuerUri" >> .envRegistry
+  echo "JAVA_TOOL_OPTIONS=$JAVA_TOOL_OPTIONS" >> .envRegistry
+}
+
+function generateUiVars(){
+  USER=$(whoami)
+  if [ "$USER" = "gitpod" ]; then
+    REACT_CONFIG_TERRAKUBE_URL=$(gp url 8080) 
+    REDIRECT=$(gp url 3000)
+    REACT_CONFIG_REGISTRY_URI=$(gp url 8075)  
+  else
+    REACT_CONFIG_TERRAKUBE_URL="http://localhost:8080"
+    REDIRECT="http://localhost:3000"
+    REACT_CONFIG_REGISTRY_URI="http://localhost:8075" 
+  fi
+
+  REACT_CONFIG_CLIENT_ID="example-app"
+  REACT_CONFIG_AUTHORITY="https://login.microsoftonline.com/89a1b398-5a8d-4060-b3e4-9d7b849527a9"
+  REACT_CONFIG_SCOPE="api://azbuilder/Builder.Default "
+
+  rm -f .envUi
+
+  echo "REACT_CONFIG_TERRAKUBE_URL=$REACT_CONFIG_TERRAKUBE_URL" >> .envUi
+  echo "REACT_CONFIG_CLIENT_ID=$REACT_CONFIG_CLIENT_ID" >> .envUi
+  echo "REACT_CONFIG_AUTHORITY=$REACT_CONFIG_AUTHORITY" >> .envUi
+  echo "REACT_CONFIG_REGISTRY_URI=$REACT_CONFIG_REGISTRY_URI" >> .envUi
+  echo "REACT_CONFIG_SCOPE=$REACT_CONFIG_SCOPE" >> .envUi
+
+  generateUiConfigFile
+}
+
+function generateUiConfigFile(){
+  # Recreate config file
+  rm -rf ui/env-config.js
+  touch ui/env-config.js
+
+  # Add assignment
+  echo "window._env_ = {" >> ui/env-config.js
+
+  # Read each line in .env file
+  # Each line represents key=value pairs
+  while read -r line || [[ -n "$line" ]];
+  do
+    # Split env variables by character `=`
+    if printf '%s\n' "$line" | grep -q -e '='; then
+      varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
+      varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+    fi
+
+    # Read value of current variable if exists as Environment variable
+    value=$(printf '%s\n' "${!varname}")
+    # Otherwise use value from .env file
+    [[ -z $value ]] && value=${varvalue}
+
+    # Append configuration property to JS file
+    echo "  $varname: \"$value\"," >> ui/env-config.js
+  done < .envUI
+
+  echo "}" >> ui/env-config.js
+
+  cp ui/env-config.js ui/public/ 
+
+}
+
+function generateDexConfiguration(){
+  cp scripts/template/dex/template-config-ldap.yaml scripts/setup/dex/config-ldap.yaml
+  
+  USER=$(whoami)
+  if [ "$USER" = "gitpod" ]; then
+    jwtIssuer=$(gp url 5556)
+    uiRedirect=$(gp url 3000)  
+  else
+    jwtIssuer="http://localhost:5556"
+    uiRedirect="http://locahost:3000"
+  fi
+  
+  sed -i "s+TEMPLATE_GITPOD_JWT_ISSUER+$jwtIssuer+gi" scripts/setup/dex/config-ldap.yaml
+  sed -i "s+TEMPLATE_GITPOD_REDIRECT+$uiRedirect+gi" scripts/setup/dex/config-ldap.yaml
+
+}
+
+generateApiVars
+generateRegistryVars
+generateExecutorVars
+generateUiVars
+generateDexConfiguration
+
+echo "Setup Development Environment Completed"
