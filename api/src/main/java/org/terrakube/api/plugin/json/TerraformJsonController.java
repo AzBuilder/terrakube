a3abeda42328c3df4669c9836fa7fdba4517e4f4
Alfredo España <alfespa17@gmail.com>
2022-08-18 11:38:11 -0600

    Change logic

--- api/src/main/java/org/terrakube/api/plugin/json/TerraformJsonController.java
+++ api/src/main/java/org/terrakube/api/plugin/json/TerraformJsonController.java
@@ -1,33 +1,24 @@
 package org.terrakube.api.plugin.json;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.time.Duration;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.Charset;
 
 @Slf4j
 @RestController
 @RequestMapping("/terraform")
 public class TerraformJsonController {
 
-    @GetMapping("/index.json")
-    public ResponseEntity<String> createToken() {
-        WebClient client = WebClient.builder()
-                .baseUrl("https://releases.hashicorp.com")
-                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-
-        String terraformIndex = client.get()
-                .uri("/terraform/index.json")
-                .retrieve()
-                .bodyToMono(String.class)
-                .timeout(Duration.ofSeconds(10))
-                .block();
+    @GetMapping(value= "/index.json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> createToken() throws IOException {
+        String terraformIndex = IOUtils.toString(URI.create("https://releases.hashicorp.com/terraform/index.json"), Charset.defaultCharset());
         return new ResponseEntity<>(terraformIndex, HttpStatus.OK);
     }
 }
