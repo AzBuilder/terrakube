4a1f14672f2af0abb2d112efe0e533f30f8f220e
Alfredo España <alfespa17@gmail.com>
2022-08-18 11:28:27 -0600

    Add endpoint for terraform versions

---
+++ api/src/main/java/org/terrakube/api/plugin/json/TerraformJsonController.java
@@ -0,0 +1,35 @@
+package org.terrakube.api.plugin.json;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+
+@Slf4j
+@RestController
+@RequestMapping("/terraform")
+public class TerraformJsonController {
+
+    @GetMapping("/index.json")
+    public ResponseEntity<String> createToken() {
+        WebClient client = WebClient.builder()
+                .baseUrl("https://releases.hashicorp.com")
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        String terraformIndex = client.post()
+                .uri("/terraform/index.json")
+                .retrieve()
+                .bodyToMono(String.class)
+                .timeout(Duration.ofSeconds(10))
+                .block();
+        return new ResponseEntity<>(terraformIndex, HttpStatus.OK);
+    }
+}
+
+
