bcfab276c000a775b9617763c33d6767d5aa60b8
Alfredo España <alfespa17@gmail.com>
2022-10-13 18:12:29 +0000

    Small fix logic

--- api/src/main/java/org/terrakube/api/plugin/storage/gcp/GcpStorageTypeServiceImpl.java
+++ api/src/main/java/org/terrakube/api/plugin/storage/gcp/GcpStorageTypeServiceImpl.java
@@ -22,9 +22,9 @@ public class GcpStorageTypeServiceImpl implements StorageTypeService {
 
     private static final String TERRAFORM_PLAN_FILE = "terraformLibrary.tfPlan";
     private static final String GCP_LOCATION_OUTPUT = "tfoutput/%s/%s/%s.tfoutput";
-    private static final String GCP_STATE_LOCATION  = "tfstate/%s/%s/%s/%s/" + TERRAFORM_PLAN_FILE;
-    private static final String GCP_STATE_JSON      = "tfstate/%s/%s/state/%s.json";
-    private static final String CONTEXT_JSON        = "tfoutput/context/%s/context.json";
+    private static final String GCP_STATE_LOCATION = "tfstate/%s/%s/%s/%s/" + TERRAFORM_PLAN_FILE;
+    private static final String GCP_STATE_JSON = "tfstate/%s/%s/state/%s.json";
+    private static final String CONTEXT_JSON = "tfoutput/context/%s/context.json";
 
     @NonNull
     private String bucketName;
@@ -35,9 +35,9 @@ public class GcpStorageTypeServiceImpl implements StorageTypeService {
     public byte[] getStepOutput(String organizationId, String jobId, String stepId) {
         log.info("getStepOutput {}", String.format(GCP_LOCATION_OUTPUT, organizationId, jobId, stepId));
         return storage.get(
-                BlobId.of(
-                        bucketName,
-                        String.format(GCP_LOCATION_OUTPUT, organizationId, jobId, stepId)))
+                        BlobId.of(
+                                bucketName,
+                                String.format(GCP_LOCATION_OUTPUT, organizationId, jobId, stepId)))
                 .getContent();
     }
 
@@ -45,9 +45,9 @@ public class GcpStorageTypeServiceImpl implements StorageTypeService {
     public byte[] getTerraformPlan(String organizationId, String workspaceId, String jobId, String stepId) {
         log.info("getTerraformPlan {}", String.format(GCP_STATE_LOCATION, organizationId, workspaceId, jobId, stepId));
         return storage.get(
-                BlobId.of(
-                        bucketName,
-                        String.format(GCP_STATE_LOCATION, organizationId, workspaceId, jobId, stepId)))
+                        BlobId.of(
+                                bucketName,
+                                String.format(GCP_STATE_LOCATION, organizationId, workspaceId, jobId, stepId)))
                 .getContent();
     }
 
@@ -55,9 +55,9 @@ public class GcpStorageTypeServiceImpl implements StorageTypeService {
     public byte[] getTerraformStateJson(String organizationId, String workspaceId, String stateFileName) {
         log.info("getTerraformStateJson {}", String.format(GCP_STATE_JSON, organizationId, workspaceId, stateFileName));
         return storage.get(
-                BlobId.of(
-                        bucketName,
-                        String.format(GCP_STATE_JSON, organizationId, workspaceId, stateFileName)))
+                        BlobId.of(
+                                bucketName,
+                                String.format(GCP_STATE_JSON, organizationId, workspaceId, stateFileName)))
                 .getContent();
     }
 
@@ -89,10 +89,14 @@ public class GcpStorageTypeServiceImpl implements StorageTypeService {
     @Override
     public String getContext(int jobId) {
         log.info("context {}", String.format(CONTEXT_JSON, jobId));
-        return new String(storage.get(
-                        BlobId.of(
-                                bucketName,
-                                String.format(CONTEXT_JSON, jobId)))
-                .getContent(), StandardCharsets.UTF_8);
+
+        if (storage.get(BlobId.of(bucketName, String.format(CONTEXT_JSON, jobId))) != null)
+            return new String(storage.get(
+                            BlobId.of(
+                                    bucketName,
+                                    String.format(CONTEXT_JSON, jobId)))
+                    .getContent(), StandardCharsets.UTF_8);
+        else
+            return "{}";
     }
 }
