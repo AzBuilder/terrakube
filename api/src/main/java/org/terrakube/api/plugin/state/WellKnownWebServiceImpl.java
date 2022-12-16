59b58937a1a62bb8442127081475386d5ec37b42
Alfredo España <alfespa17@gmail.com>
2022-12-16 15:52:02 -0600

    Adding state endpoint

---
+++ api/src/main/java/org/terrakube/api/plugin/state/WellKnownWebServiceImpl.java
@@ -0,0 +1,26 @@
+package org.terrakube.api.plugin.state;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/.well-known/terraform.json")
+public class WellKnownWebServiceImpl {
+
+    private static final String terraformJsonContent = "{\n" +
+            "  \"state.v2\": \"%s/terrakube/state/v1/\"\n" +
+            "}";
+
+    @Value("org.terrakube.hostname")
+    String hostname;
+
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<String> terraformJson() {
+        return ResponseEntity.ok(
+                String.format(terraformJsonContent,hostname)
+        );
+    }
+}
