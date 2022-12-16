3d35b75bb47362338df2c3668d8f11732089994b
Alfredo España <alfespa17@gmail.com>
2022-12-16 16:14:49 -0600

    Add more info

--- api/src/main/java/org/terrakube/api/plugin/state/WellKnownWebServiceImpl.java
+++ api/src/main/java/org/terrakube/api/plugin/state/WellKnownWebServiceImpl.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class WellKnownWebServiceImpl {
 
     private static final String terraformJsonContent = "{\n" +
-            "  \"state.v2\": \"%s/terrakube/state/v1/\"\n" +
+            "  \"state.v2\": \"%s/terrakube/state/v1/\"\n," +
+            "  \"tfe.v2.1\": \"%s/terrakube/tfe/v1/\"\n," +
             "}";
 
     @Value("${org.terrakube.hostname}s")
