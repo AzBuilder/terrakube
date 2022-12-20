d4114ab4fe4ced50fd6d40eda0dca4ec80398ee3
Alfredo España <alfespa17@gmail.com>
2022-12-19 18:03:07 -0600

    Refactor org response

--- api/src/main/java/org/terrakube/api/plugin/state/model/organization/OrganizationModel.java
+++ api/src/main/java/org/terrakube/api/plugin/state/model/organization/OrganizationModel.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 import org.terrakube.api.plugin.state.model.generic.Resource;
 
+import java.util.Map;
+
 @Getter
 @Setter
 public class OrganizationModel extends Resource {
-    OrganizationAttributes attributes;
+    Map<String, Object> attributes;
 }
