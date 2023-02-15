59a1f67f6311117ac7a1e52cada25f1b7586a78d
Alfredo España <alfespa17@gmail.com>
2023-02-15 11:56:57 -0600

    Import template logic

--- api/src/main/java/org/terrakube/api/plugin/scheduler/job/tcl/TclService.java
+++ api/src/main/java/org/terrakube/api/plugin/scheduler/job/tcl/TclService.java
@@ -4,10 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.stereotype.Service;
-import org.terrakube.api.plugin.scheduler.job.tcl.model.Flow;
-import org.terrakube.api.plugin.scheduler.job.tcl.model.FlowConfig;
-import org.terrakube.api.plugin.scheduler.job.tcl.model.FlowType;
+import org.terrakube.api.plugin.scheduler.job.tcl.model.*;
 import org.terrakube.api.repository.JobRepository;
 import org.terrakube.api.repository.StepRepository;
 import org.terrakube.api.repository.TemplateRepository;
@@ -18,6 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.*;
 
 @AllArgsConstructor
@@ -26,6 +30,8 @@ import java.util.*;
 @Setter
 @Slf4j
 public class TclService {
+
+    private static final String IMPORT_DIRECTORY = "%s/.terraform-spring-boot/importCommands/%s";
 
     JobRepository jobRepository;
     StepRepository stepRepository;
@@ -76,7 +82,7 @@ public class TclService {
             log.info("FlowConfig: \n {}", temp);
             flowConfig = yaml.load(new String(Base64.getDecoder().decode(tcl)));
 
-            if(flowConfig.getFlow().isEmpty()){
+            if (flowConfig.getFlow().isEmpty()) {
                 log.error("Exception parsing yaml: template with no flows");
                 return setErrorFlowYaml("Yaml Template does not have any flow");
             }
@@ -113,9 +119,74 @@ public class TclService {
                     .filter(flow -> flow.getStep() == map.firstKey())
                     .findFirst();
 
+            ImportComands importComands = nextFlow.get().getImportComands();
+            if (importComands != null) {
+                log.info("Import commands from {} branch {} folder {}", importComands.getRepository(), importComands.getBranch(), importComands.getFolder());
+
+                nextFlow.get().setCommands(importCommands(importComands.getRepository(), importComands.getBranch(), importComands.getFolder()));
+            }
+
             return nextFlow.isPresent() ? nextFlow.get() : null;
         } else
             return null;
+    }
+
+    private List<Command> importCommands(String repository, String branch, String folder) {
+        List<Command> commands = new ArrayList();
+        try {
+            File importFolder = generateImportFolder();
+            String commandsText = getCommandList(repository, branch, folder, generateImportFolder());
+
+            Yaml yaml = new Yaml(new Constructor(CommandConfig.class));
+            CommandConfig temp = yaml.load(commandsText);
+
+            commands = temp.getCommands();
+
+            log.info("Importing commands \n{}\n", commandsText);
+
+            FileUtils.cleanDirectory(importFolder);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return commands;
+    }
+
+    private String getCommandList(String repository, String branch, String folder, File folderImport) {
+        String commandList = "";
+        try {
+            Git.cloneRepository()
+                    .setURI(repository)
+                    .setDirectory(folderImport)
+                    .setBranch(branch)
+                    .call();
+            File importData = null;
+            if (folder.equals("/")) {
+                importData = new File(String.format("%s/commands.yaml", folderImport.getCanonicalPath()));
+            } else {
+                importData = new File(String.format("%s/%s/commands.yaml", folderImport.getCanonicalPath(), folder));
+            }
+
+            commandList = FileUtils.readFileToString(importData, Charset.defaultCharset());
+        } catch (IOException | GitAPIException e) {
+            log.error(e.getMessage());
+        }
+        return commandList;
+    }
+
+    private File generateImportFolder() {
+        String importCommandFolder = String.format(IMPORT_DIRECTORY, FileUtils.getUserDirectoryPath(), UUID.randomUUID());
+        File importFolder = new File(importCommandFolder);
+        try {
+            if (!importFolder.exists()) {
+                log.info("Creating new import folder for {}", importCommandFolder);
+                FileUtils.forceMkdir(importFolder);
+            } else {
+                FileUtils.cleanDirectory(importFolder);
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return importFolder;
     }
 
     private TreeMap<Integer, Step> getPendingSteps(Job job) {
