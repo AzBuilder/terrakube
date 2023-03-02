da934141e94edd4aeed9f8a538b417fdfc2ad09b
Alfredo España <alfespa17@gmail.com>
2023-03-02 14:53:04 -0600

    Update devfile.yaml

--- scripts/che/buildApi.sh
+++ scripts/che/buildApi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set java 17
-sdk install java 17.0.6-tem
+# sdk install java 17.0.6-tem
 
 # Set maven 
-sdk install maven
+# sdk install maven
 
 # Build
 mvn clean install -Dmaven.test.skip=true -Dspring-boot.build-image.skip=true -f api/pom.xml 
