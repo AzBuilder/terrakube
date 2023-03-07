e309345e5016fc05feafba4ca41b1249d1ed7755
Alfredo España <alfespa17@gmail.com>
2023-03-07 15:50:35 -0600

    Initial Eclipse CHE support (#342)
    * Initial support for eclipse che development environment [skip ci]

--- scripts/dependencies/loadDex.sh
+++ scripts/dependencies/loadDex.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-cp scripts/template/dex/template-config-ldap.yaml scripts/setup/dex/config-ldap.yaml
-jwtIssuer=$(gp url 5556)
-sed -i "s+TEMPLATE_GITPOD_JWT_ISSUER+$jwtIssuer+gi" scripts/setup/dex/config-ldap.yaml
-uiRedirect=$(gp url 3000)
-sed -i "s+TEMPLATE_GITPOD_REDIRECT+$uiRedirect+gi" scripts/setup/dex/config-ldap.yaml
-docker-compose -f scripts/setup/dex/docker-compose.yaml up -d
-clear
-echo "Dex Enviroment setup completed, you can close this terminal"
+while [ ! -f /projects/terrakube/scripts/setup/dex/config-ldap.yaml ]
+do
+  sleep 2
+done
+dex serve --web-http-addr 0.0.0.0:5556 /projects/terrakube/scripts/setup/dex/config-ldap.yaml 
