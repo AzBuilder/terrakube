486dbc3671bb0181403eab607327dd9d0b55fa6c
Alfredo España <alfespa17@gmail.com>
2023-03-14 15:48:42 +0000

    Small fix logic

--- api/src/main/java/org/terrakube/api/repository/StepRepository.java
+++ api/src/main/java/org/terrakube/api/repository/StepRepository.java
@@ -4,8 +4,9 @@ import org.terrakube.api.rs.job.step.Step;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface StepRepository extends JpaRepository<Step, Integer> {
+public interface StepRepository extends JpaRepository<Step, UUID> {
 
     List<Step> findByJobId(int jobId);
 }
