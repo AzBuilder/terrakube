9401547c9ac1bc014e56f29a4b4a97f2f4dc493b
Alfredo España <alfespa17@gmail.com>
2023-04-11 00:12:21 +0000

    Small changes

--- api/src/main/java/org/terrakube/api/plugin/streaming/StreamingService.java
+++ api/src/main/java/org/terrakube/api/plugin/streaming/StreamingService.java
@@ -3,6 +3,7 @@ package org.terrakube.api.plugin.streaming;
 import liquibase.repackaged.org.apache.commons.text.TextStringBuilder;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.connection.stream.StreamOffset;
 import org.springframework.data.redis.connection.stream.StringRecord;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -21,14 +22,17 @@ public class StreamingService {
 
         TextStringBuilder logs = new TextStringBuilder();
         try {
-            List<StringRecord> streamData = redisTemplate.opsForStream().read(StreamOffset.fromStart(stepId), StreamOffset.latest(stepId));
+            List<MapRecord> streamData = redisTemplate.opsForStream().read(StreamOffset.fromStart(stepId), StreamOffset.latest(stepId));
 
-            for (StringRecord record : streamData) {
-                logs.appendln(record.getValue().get("output"));
+            for (MapRecord record : streamData) {
+                StringRecord record1 = StringRecord.of(record);
+                String output = record1.getValue().get("output");
+                log.info("{}", output);
+                logs.appendln(output);
             }
-            log.info("{}", logs.toString());
         } catch (Exception ex ){
             log.error(ex.getMessage());
+
         }
         return logs.toString();
     }
