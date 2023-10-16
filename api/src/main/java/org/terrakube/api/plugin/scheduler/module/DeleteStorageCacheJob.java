ed35124332320326a5d36560f022ea08ddf18577
Alfredo España <alfespa17@gmail.com>
2023-10-16 21:31:23 +0000

    Small fix

--- api/src/main/java/org/terrakube/api/plugin/scheduler/module/DeleteStorageCacheJob.java
+++ api/src/main/java/org/terrakube/api/plugin/scheduler/module/DeleteStorageCacheJob.java
@@ -1,12 +1,16 @@
 package org.terrakube.api.plugin.scheduler.module;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.stereotype.Component;
 import org.terrakube.api.plugin.storage.StorageTypeService;
 
 @Slf4j
+@AllArgsConstructor
+@Component
 public class DeleteStorageCacheJob implements Job {
 
     StorageTypeService storageTypeService;
