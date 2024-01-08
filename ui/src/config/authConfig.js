df56b30882c983902ca1ad2bb5012c89ee6bdb93
Ilker Ispir <ikerispir@gmail.com>
2024-01-09 06:30:49 +1000

    feat: migrating oidc.user token information from sessionstorage to localstorage (#666)

--- ui/src/config/authConfig.js
+++ ui/src/config/authConfig.js
@@ -1,4 +1,4 @@
-import { UserManager } from 'oidc-client-ts';
+import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
 import { AuthContext } from 'react-oidc-context';
 import React from "react";
 
@@ -6,7 +6,8 @@ export const oidcConfig = {
   authority: window._env_.REACT_APP_AUTHORITY,
   client_id: window._env_.REACT_APP_CLIENT_ID,
   redirect_uri: window._env_.REACT_APP_REDIRECT_URI,
-  scope: window._env_.REACT_APP_SCOPE 
+  scope: window._env_.REACT_APP_SCOPE,
+  userStore: new WebStorageStateStore({ store: window.localStorage })
 }
 
 export const mgr = new UserManager(oidcConfig);
