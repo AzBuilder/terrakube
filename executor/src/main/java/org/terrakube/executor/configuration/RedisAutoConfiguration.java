1dea6b3c42dfb576d64bb4c26e16c39a2f2620e3
Roger Miret <rogerxaic@users.noreply.github.com>
2024-02-15 22:45:40 +0100

    Redis SSL improvement (+postgres fix) (#713)
    * change liquibase defaultSchema (+some cleaning)
    
    * redis: add user parameter and possibility to use SSL server

--- executor/src/main/java/org/terrakube/executor/configuration/RedisAutoConfiguration.java
+++ executor/src/main/java/org/terrakube/executor/configuration/RedisAutoConfiguration.java
@@ -1,19 +1,74 @@
 package org.terrakube.executor.configuration;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 public class RedisAutoConfiguration {
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory(RedisProperties redisProperties) {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisProperties.getHostname(), redisProperties.getPort());
-        redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
-        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory(redisStandaloneConfiguration);
+    SSLSocketFactory sslSocketFactory(RedisProperties props) throws Exception {
+        if (!props.isSsl()) {
+            return SSLContext.getDefault().getSocketFactory();
+        }
+        KeyStore trustStore = KeyStore.getInstance("jks");
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(props.getTruststorePath());
+            trustStore.load(inputStream, props.getTruststorePassword().toCharArray());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("PKIX");
+        trustManagerFactory.init(trustStore);
+        TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustManagers, new SecureRandom());
+        return sslContext.getSocketFactory();
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory(RedisProperties props, SSLSocketFactory sslSocketFactory) {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
+                props.getHostname(), props.getPort());
+        redisStandaloneConfiguration.setPassword(props.getPassword());
+        redisStandaloneConfiguration.setUsername(props.getUsername());
+
+        JedisConnectionFactory jedisConFactory;
+
+        if (props.isSsl()) {
+            JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder().useSsl()
+                    .sslSocketFactory(sslSocketFactory).build();
+
+            jedisConFactory = new JedisConnectionFactory(redisStandaloneConfiguration,
+                    jedisClientConfiguration);
+        } else {
+            jedisConFactory = new JedisConnectionFactory(redisStandaloneConfiguration);
+        }
+
         return jedisConFactory;
     }
 
