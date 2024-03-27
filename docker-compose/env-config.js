74315b5bf399ed62b9b715022e1b3ecdbf1eb731
Alfredo España <alfespa17@gmail.com>
2024-03-27 13:46:06 -0600

    Update docker compose to 2.20.0 [skip ci]

--- docker-compose/env-config.js
+++ docker-compose/env-config.js
@@ -5,4 +5,5 @@ window._env_ = {
   REACT_APP_REDIRECT_URI: "http://terrakube-ui:3000",
   REACT_APP_REGISTRY_URI: "http://terrakube-registry:8075",
   REACT_APP_SCOPE: "email openid profile offline_access groups",
+  REACT_APP_TERRAKUBE_VERSION: "Docker Compose Version",
 }
