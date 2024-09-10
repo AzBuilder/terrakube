d87ee23c4b10627d255442afc1444b63a6cd1267
Javier Canizalez <javier-canizalez@outlook.com>
2024-09-10 21:01:12 +0000

    feat: Added Rollback State

--- ui/src/domain/Workspaces/States.jsx
+++ ui/src/domain/Workspaces/States.jsx
@@ -1,5 +1,16 @@
 import { React, useState, useRef, useCallback, useMemo } from "react";
-import { List, Space, Card, Row, Col, Avatar, Tooltip } from "antd";
+import {
+  List,
+  Space,
+  Card,
+  Row,
+  Col,
+  Avatar,
+  Tooltip,
+  Button,
+  Popconfirm,
+  message,
+} from "antd";
 import Editor from "@monaco-editor/react";
 import axiosInstance, { axiosClient } from "../../config/axiosConfig";
 import ReactFlow, {
@@ -10,7 +21,11 @@ import ReactFlow, {
 } from "reactflow";
 import NodeResource from "./NodeResource";
 import { DownloadState } from "./DownloadState";
-import { InfoCircleOutlined, UserOutlined } from "@ant-design/icons";
+import {
+  InfoCircleOutlined,
+  UserOutlined,
+  RollbackOutlined,
+} from "@ant-design/icons";
 import "reactflow/dist/style.css";
 import { ResourceDrawer } from "../Workspaces/ResourceDrawer";
 
@@ -20,7 +35,8 @@ export const States = ({
   stateDetailsVisible,
   workspace,
   organizationId,
-  organizationName
+  organizationName,
+  onRollback,
 }) => {
   const [currentState, setCurrentState] = useState({});
   const [stateContent, setStateContent] = useState("");
@@ -244,15 +260,44 @@ export const States = ({
     []
   );
 
+  const handleRollback = () => {
+    const outputUrl = currentState.output;
+    const rollbackUrl = outputUrl.replace("/state/", "/rollback/"); // Replace /state/ with /rollback/
+
+    axiosInstance
+      .put(rollbackUrl)
+      .then((response) => {
+        // Show success message on successful rollback
+        message.success(
+          "The state was successfully rolled back. Please verify that the workspace version is compatible with this state."
+        );
+
+        console.log("Rollback successful:", response.data);
+        onRollback(false, false);
+        setStateDetailsVisible(false);
+      })
+      .catch((error) => {
+        // Extract error message from the API response
+        const errorMessage =
+          error.response?.data || "An unexpected error occurred.";
+
+        // Show the error message
+        message.error(`Failed to roll back the state: ${errorMessage}`);
+
+        // Log the error for debugging purposes
+        console.error("Error during rollback:", error);
+      });
+  };
+
   return (
     <div>
       {!stateDetailsVisible ? (
         <List
           split=""
           className="moduleList"
-          dataSource={history
-            .sort((a, b) => a.jobReference - b.jobReference)
-            .reverse()}
+          dataSource={history.sort(
+            (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
+          )}
           renderItem={(item) => (
             <List.Item>
               <Card
@@ -267,7 +312,13 @@ export const States = ({
                     {item.relativeDate}
                   </span>
                   <span>
-                    <a>job #{item.jobReference}</a>
+                    <a>
+                      {/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(
+                        item.jobReference
+                      )
+                        ? `rollback to #${item.jobReference}`
+                        : `job #${item.jobReference}`}
+                    </a>
                   </span>
                 </Space>
               </Card>
@@ -280,22 +331,51 @@ export const States = ({
             <Col span={1}>
               <Avatar shape="square" icon={<UserOutlined />} />
             </Col>
-            <Col span={21}>
+            <Col span={19}>
               <h3>{currentState.title}</h3>
               <Space className="stateDetails" size={40} split="|">
                 <span>#{currentState.id}</span>
                 <span>
-                  <b>{currentState.createdBy}</b> triggered from Terraform
+                  <b>{currentState.createdBy}</b> triggered from Terraform{" "}
+                  <span className="stateDetails">
+                    {currentState.relativeDate}
+                  </span>
                 </span>
                 <span>
-                  <a>job #{currentState.jobReference}</a>
+                  <a>
+                    {/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(
+                      currentState.jobReference
+                    )
+                      ? `rollback to #${currentState.jobReference}`
+                      : `job #${currentState.jobReference}`}
+                  </a>
                 </span>
               </Space>
             </Col>
-            <Col span={2}>
-              <DownloadState stateUrl={currentState.output} />
-              <br />
-              <span className="stateDetails">{currentState.relativeDate}</span>
+            <Col span={3}>
+              <Space style={{ marginTop: "30px" }} direction="horizontal">
+                <Popconfirm
+                  title="Are you sure?"
+                  description={
+                    <span>
+                      Restoring this workspace to its previous state may lead to
+                      loss of data. <br /> Any resources that have been added or
+                      modified since this state was saved <br /> will no longer
+                      be tracked by Terrakube.
+                    </span>
+                  }
+                  onConfirm={handleRollback}
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <Tooltip title="Rollback to this State Version">
+                    <Button icon={<RollbackOutlined />} danger type="default">
+                      Rollback
+                    </Button>
+                  </Tooltip>
+                </Popconfirm>
+                <DownloadState stateUrl={currentState.output} />
+              </Space>
             </Col>
           </Row>
           <Row style={{ paddingTop: "30px" }}>
