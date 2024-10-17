fa19b1fe9118dfe3af862f4795e0d648f48e9465
Dawid Malinowski <dawidmalina@gmail.com>
2024-10-17 09:43:41 +0200

    fix: for annotations has been deprecated

--- registry/src/main/java/org/terrakube/registry/configuration/authentication/local/LocalWebSecurityAdapter.java
+++ registry/src/main/java/org/terrakube/registry/configuration/authentication/local/LocalWebSecurityAdapter.java
@@ -3,6 +3,7 @@ package org.terrakube.registry.configuration.authentication.local;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,9 +15,10 @@ public class LocalWebSecurityAdapter{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().authorizeRequests(authz -> {
-                            authz.requestMatchers("/**").permitAll();
-                        });
+        http.cors(Customizer.withDefaults())
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/**").permitAll()
+            );
 
         return http.build();
     }
