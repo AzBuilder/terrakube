1a3234e7264d010ea72f0713835e5b720cfb2e36
Alfredo España <alfespa17@gmail.com>
2024-11-27 13:48:48 -0600

    WIP

--- api/src/test/java/org/terrakube/api/VcsTests.java
+++ api/src/test/java/org/terrakube/api/VcsTests.java
@@ -46,7 +46,7 @@ class VcsTests extends ServerApplicationTests{
 
     @Test
     void createVcsAsOrgMember() {
-        given()
+        String vcsId = given()
                 .headers("Authorization", "Bearer " + generatePAT("TERRAKUBE_DEVELOPERS"), "Content-Type", "application/vnd.api+json")
                 .body("{\n" +
                         "  \"data\": {\n" +
@@ -68,7 +68,17 @@ class VcsTests extends ServerApplicationTests{
                 .body("data.attributes.name", IsEqual.equalTo("githubConnection"))
                 .log()
                 .all()
-                .statusCode(HttpStatus.CREATED.value());
+                .statusCode(HttpStatus.CREATED.value()).extract().path("data.id");
+
+        given()
+                .headers("Authorization", "Bearer " + generatePAT("TERRAKUBE_DEVELOPERS"))
+                .when()
+                .delete("/api/v1/organization/d9b58bd3-f3fc-4056-a026-1163297e80a8/vcs/" + vcsId)
+                .then()
+                .assertThat()
+                .log()
+                .all()
+                .statusCode(HttpStatus.NO_CONTENT.value());
     }
     @Test
     void createVcsAsNonOrgMember() {
@@ -172,5 +182,16 @@ class VcsTests extends ServerApplicationTests{
                 .log()
                 .all()
                 .statusCode(HttpStatus.OK.value());
+
+        // Delete the test vcs connection
+        given()
+                .headers("Authorization", "Bearer " + generatePAT("TERRAKUBE_DEVELOPERS"))
+                .when()
+                .delete("/api/v1/organization/d9b58bd3-f3fc-4056-a026-1163297e80a8/vcs/"+vcsId)
+                .then()
+                .assertThat()
+                .log()
+                .all()
+                .statusCode(HttpStatus.NO_CONTENT.value());
     }
 }
--- ui/src/domain/Workspaces/Details.jsx
+++ ui/src/domain/Workspaces/Details.jsx
@@ -390,9 +390,9 @@ export const WorkspaceDetails = ({ setOrganizationName, selectedTab }) => {
     });
   };
   
-  const loadPermissionSet = () => {
+  const loadPermissionSet = (workspaceId) => {
     console.log("Loading Permission Values")
-    const url = `${new URL(window._env_.REACT_APP_TERRAKUBE_API_URL).origin}/access-token/v1/teams/permissions/organization/${organizationId}/workspace/${id}`;
+    const url = `${new URL(window._env_.REACT_APP_TERRAKUBE_API_URL).origin}/access-token/v1/teams/permissions/organization/${organizationId}/workspace/${workspaceId}`;
     axiosInstance.get(url).then((response) => {
       console.log(response.data)
       setManageState(response.data.manageState);
@@ -416,7 +416,7 @@ export const WorkspaceDetails = ({ setOrganizationName, selectedTab }) => {
               loadVersions(response.data.data.attributes.iacType);
 
             if (_loadPermissionSet)
-              loadPermissionSet()
+              loadPermissionSet(id)
 
             setWorkspace(response.data);
             console.log(response.data);
@@ -1843,7 +1843,7 @@ function setupWorkspaceIncludes(
   console.log("Get latest state");
   if (currentStateId !== lastState?.id) {
     var organizationId = sessionStorage.getItem(ORGANIZATION_ARCHIVE);
-    const url = `${new URL(window._env_.REACT_APP_TERRAKUBE_API_URL).origin}/access-token/v1/teams/permissions/organization/${organizationId}/workspace/${id}`;
+    const url = `${new URL(window._env_.REACT_APP_TERRAKUBE_API_URL).origin}/access-token/v1/teams/permissions/organization/${organizationId}`;
     axiosInstance.get(url).then((response) => {
       console.log(`Manage Permission Set: ${response.data}`)
       console.log(response.data)
