5f6ffc46a912879ef88ed2686653184bb5b97b38
Alfredo España <alfespa17@gmail.com>
2025-03-07 15:16:07 -0600

    feat: Update docker compose to 2.25.0 using traefik with https

--- docker-compose/env-config.js
+++ docker-compose/env-config.js
@@ -1,9 +1,8 @@
 window._env_ = {
-  REACT_APP_TERRAKUBE_API_URL: "http://terrakube-api:8080/api/v1/",
-  REACT_APP_CLIENT_ID: "example-app",
-  REACT_APP_AUTHORITY: "http://terrakube-dex:5556/dex",
-  REACT_APP_REDIRECT_URI: "http://terrakube-ui:3000",
-  REACT_APP_REGISTRY_URI: "http://terrakube-registry:8075",
-  REACT_APP_SCOPE: "email openid profile offline_access groups",
-  REACT_APP_TERRAKUBE_VERSION: "Docker Compose Version",
+    REACT_APP_TERRAKUBE_API_URL: "https://terrakube-api.platform.local/api/v1/",
+    REACT_APP_CLIENT_ID: "terrakube-app",
+    REACT_APP_AUTHORITY: "https://terrakube-dex.platform.local/dex",
+    REACT_APP_REDIRECT_URI: "https://terrakube.platform.local",
+    REACT_APP_REGISTRY_URI: "https://terrakube-registry.platform.local",
+    REACT_APP_SCOPE: "email openid profile offline_access groups",
 }
