de6c43bec8286b778d5e39ecd87745e1ef3e0779
Joachim Dalen <me@d3v.no>
2025-03-16 10:10:42 +0100

    refactor login

--- ui/src/domain/Login/Login.tsx
+++ ui/src/domain/Login/Login.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import './Login.css';
-import { Card ,Space} from "antd";
-import logo from './logo.jpg';
+import { Button, Card, Space } from "antd";
 import { mgr } from "../../config/authConfig";
-import { Button } from "antd";
+import "./Login.css";
+import logo from "./logo.jpg";
+
 const Login = () => {
-
   return (
-    <div className='login-container'>
-      <div className='login-wrapper'>
-
-        <Card title={<img alt="logo"  className="loginLogo" src={logo} />}>
+    <div className="login-container">
+      <div className="login-wrapper">
+        <Card title={<img alt="logo" className="loginLogo" src={logo} />}>
           <Space direction="vertical">
-          Sign in to Terrakube
-          <Button type="primary" onClick={() => App()}>Login</Button>
+            Sign in to Terrakube
+            <Button type="primary" onClick={() => App()}>
+              Login
+            </Button>
           </Space>
         </Card>
-
-      </div> </div>
-  )
-}
+      </div>{" "}
+    </div>
+  );
+};
 
 function App() {
   console.log(mgr);
