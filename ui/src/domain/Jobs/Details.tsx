9e0310438be10f70667e4daae28a9f9560043511
Joachim Dalen <me@d3v.no>
2025-03-16 10:07:50 +0100

    refactor jobs

--- ui/src/domain/Jobs/Details.tsx
+++ ui/src/domain/Jobs/Details.tsx
@@ -1,71 +1,66 @@
-import { React, useState, useEffect } from "react";
-import {
-  Tag,
-  Space,
-  Collapse,
-  Avatar,
-  Card,
-  Button,
-  message,
-  Radio,
-  Spin,
-} from "antd";
-import { ORGANIZATION_ARCHIVE } from "../../config/actionTypes";
-import axiosInstance, { axiosClient } from "../../config/axiosConfig";
 import {
   CheckCircleOutlined,
   CheckCircleTwoTone,
-  SyncOutlined,
-  ClockCircleOutlined,
-  ExclamationCircleOutlined,
   CheckOutlined,
+  ClockCircleOutlined,
+  CloseCircleTwoTone,
   CloseOutlined,
   CommentOutlined,
-  CloseCircleTwoTone,
+  ExclamationCircleOutlined,
   StopOutlined,
+  SyncOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 import Ansi from "ansi-to-react";
+import { Avatar, Button, Card, Collapse, message, Radio, RadioChangeEvent, Space, Spin, Tag } from "antd";
+import { AxiosResponse } from "axios";
 import parse from "html-react-parser";
+import { useEffect, useState } from "react";
+import { ORGANIZATION_ARCHIVE } from "../../config/actionTypes";
+import axiosInstance, { axiosClient } from "../../config/axiosConfig";
+import { Job, JobStep } from "../types";
 
 const { DateTime } = require("luxon");
 
-export const DetailsJob = ({ jobId }) => {
+type Props = {
+  jobId: string;
+};
+
+export const DetailsJob = ({ jobId }: Props) => {
   const organizationId = sessionStorage.getItem(ORGANIZATION_ARCHIVE);
   const [loading, setLoading] = useState(false);
-  const [job, setJob] = useState([]);
-  const [steps, setSteps] = useState([]);
+  const [job, setJob] = useState<AxiosResponse<Job>>();
+  const [steps, setSteps] = useState<JobStep[]>([]);
   const [uiType, setUIType] = useState("structured");
-  const [uiTemplates, setUITemplates] = useState({});
-  const outputLog = async (output, status) => {
+  const [uiTemplates, setUITemplates] = useState<Record<number, string>>({});
+  const outputLog = async (output: string, status: string) => {
     if (output != null) {
-      const apiDomain = new URL(window._env_.REACT_APP_TERRAKUBE_API_URL)
-        .hostname;
+      const apiDomain = new URL(window._env_.REACT_APP_TERRAKUBE_API_URL).hostname;
       if (output.includes(apiDomain))
         return axiosInstance
           .get(output)
           .then((resp) => resp.data)
-          .catch((err) => "No logs available");
+          .catch(() => "No logs available");
       else
         return axiosClient
           .get(output)
           .then((resp) => resp.data)
-          .catch((err) => "No logs available");
+          .catch(() => "No logs available");
     } else {
       if (status === "running") return "Initializing the backend...";
       else return "Waiting logs...";
     }
   };
 
-  const handleComingSoon = (e) => {
+  const handleComingSoon = () => {
     message.info("Coming Soon!");
   };
 
-  const onChange = (e) => {
+  const onChange = (e: RadioChangeEvent) => {
     setUIType(e.target.value);
   };
 
-  const handleCancel = (e) => {
+  const handleCancel = () => {
     const body = {
       data: {
         type: "job",
@@ -82,59 +77,32 @@ export const DetailsJob = ({ jobId }) => {
           "Content-Type": "application/vnd.api+json",
         },
       })
-      .then((response) => {
+      .then(() => {
         message.success("Job Cancelled Succesfully");
         loadJob();
       });
   };
 
-  const getIconStatus = (item) => {
+  const getIconStatus = (item: JobStep) => {
     switch (item.status) {
       case "completed":
-        return (
-          <CheckCircleTwoTone
-            twoToneColor="#52c41a"
-            style={{ fontSize: "20px" }}
-          />
-        );
+        return <CheckCircleTwoTone twoToneColor="#52c41a" style={{ fontSize: "20px" }} />;
       case "noChanges":
-        return (
-            <CheckCircleTwoTone
-                twoToneColor="#52c41a"
-                style={{ fontSize: "20px" }}
-            />
-        );
+        return <CheckCircleTwoTone twoToneColor="#52c41a" style={{ fontSize: "20px" }} />;
       case "notExecuted":
-        return (
-            <CheckCircleTwoTone
-                twoToneColor="#fa8f37"
-                style={{ fontSize: "20px" }}
-            />
-        );
+        return <CheckCircleTwoTone twoToneColor="#fa8f37" style={{ fontSize: "20px" }} />;
       case "running":
-        return (
-          <SyncOutlined spin style={{ color: "#108ee9", fontSize: "20px" }} />
-        );
+        return <SyncOutlined spin style={{ color: "#108ee9", fontSize: "20px" }} />;
       case "failed":
-        return (
-          <CloseCircleTwoTone
-            twoToneColor="#FB0136"
-            style={{ fontSize: "20px" }}
-          />
-        );
+        return <CloseCircleTwoTone twoToneColor="#FB0136" style={{ fontSize: "20px" }} />;
       case "cancelled":
-        return (
-          <CloseCircleTwoTone
-            twoToneColor="#FB0136"
-            style={{ fontSize: "20px" }}
-          />
-        );
+        return <CloseCircleTwoTone twoToneColor="#FB0136" style={{ fontSize: "20px" }} />;
       default:
         return <ClockCircleOutlined style={{ fontSize: "20px" }} />;
     }
   };
 
-  const handleApprove = (e) => {
+  const handleApprove = () => {
     const body = {
       data: {
         type: "job",
@@ -156,7 +124,7 @@ export const DetailsJob = ({ jobId }) => {
       });
   };
 
-  const handleRejected = (e) => {
+  const handleRejected = () => {
     const body = {
       data: {
         type: "job",
@@ -178,7 +146,7 @@ export const DetailsJob = ({ jobId }) => {
       });
   };
 
-  const sortbyName = (a, b) => {
+  const sortbyName = (a: JobStep, b: JobStep) => {
     if (a.stepNumber < b.stepNumber) return -1;
     if (a.stepNumber > b.stepNumber) return 1;
     return 0;
@@ -197,49 +165,41 @@ export const DetailsJob = ({ jobId }) => {
   }, [jobId]);
 
   const loadJob = () => {
-    let jobSteps = [];
-    axiosInstance
-      .get(`organization/${organizationId}/job/${jobId}?include=step`)
-      .then((response) => {
-        (async () => {
-          setJob(response.data);
+    let jobSteps: JobStep[] = [];
+    axiosInstance.get(`organization/${organizationId}/job/${jobId}?include=step`).then((response) => {
+      (async () => {
+        setJob(response.data);
 
-          if (response.data.included != null) {
-            for (const element of response.data.included) {
-              let log = await outputLog(
-                element.attributes.output,
-                element.attributes.status
-              );
-              jobSteps.push({
-                id: element.id,
-                stepNumber: element.attributes.stepNumber,
-                status: element.attributes.status,
-                output: element.attributes.output,
-                name: element.attributes.name,
-                outputLog: log,
-              });
-            }
+        if (response.data.included != null) {
+          for (const element of response.data.included) {
+            let log = await outputLog(element.attributes.output, element.attributes.status);
+            jobSteps.push({
+              id: element.id,
+              stepNumber: element.attributes.stepNumber,
+              status: element.attributes.status,
+              output: element.attributes.output,
+              name: element.attributes.name,
+              outputLog: log,
+            });
           }
-          setSteps(jobSteps.sort(sortbyName));
-        })();
-      });
+        }
+        setSteps(jobSteps.sort(sortbyName));
+      })();
+    });
   };
 
   const loadContext = () => {
     const api = new URL(window._env_.REACT_APP_TERRAKUBE_API_URL);
 
-    axiosInstance
-      .get(`${api.protocol}//${api.host}/context/v1/${jobId}`)
-      .then((response) => {
-        console.log("terrakube");
-        console.log(response?.data?.terrakubeUI);
-        if (response?.data?.terrakubeUI)
-          setUITemplates(response?.data?.terrakubeUI);
-      });
+    axiosInstance.get(`${api.protocol}//${api.host}/context/v1/${jobId}`).then((response) => {
+      console.log("terrakube");
+      console.log(response?.data?.terrakubeUI);
+      if (response?.data?.terrakubeUI) setUITemplates(response?.data?.terrakubeUI);
+    });
   };
   return (
     <div style={{ marginTop: "14px" }}>
-      {loading || !job.data || !steps ? (
+      {loading || !job?.data || !steps ? (
         <Spin spinning={true} tip="Loading Job...">
           <p style={{ marginTop: "50px" }}></p>
         </Spin>
@@ -264,10 +224,10 @@ export const DetailsJob = ({ jobId }) => {
               }
               color={
                 job.data.attributes.status === "completed"
-                  ? "#2eb039":
-                  job.data.attributes.status === "noChanges"
-                  ? "#2eb039" :
-                  job.data.attributes.status === "notExecuted"
+                  ? "#2eb039"
+                  : job.data.attributes.status === "noChanges"
+                  ? "#2eb039"
+                  : job.data.attributes.status === "notExecuted"
                   ? "#fa8f37"
                   : job.data.attributes.status === "running"
                   ? "#108ee9"
@@ -291,16 +251,9 @@ export const DetailsJob = ({ jobId }) => {
                 key: "1",
                 label: (
                   <span>
-                    <Avatar
-                      size="small"
-                      shape="square"
-                      icon={<UserOutlined />}
-                    />{" "}
-                    <b>{job.data.attributes.createdBy}</b> triggered a run from{" "}
-                    {job.data.attributes.via || "UI"}{" "}
-                    {DateTime.fromISO(
-                      job.data.attributes.createdDate
-                    ).toRelative()}
+                    <Avatar size="small" shape="square" icon={<UserOutlined />} />{" "}
+                    <b>{job.data.attributes.createdBy}</b> triggered a run from {job.data.attributes.via || "UI"}{" "}
+                    {DateTime.fromISO(job.data.attributes.createdDate).toRelative()}
                   </span>
                 ),
                 children: <p></p>,
@@ -342,17 +295,9 @@ export const DetailsJob = ({ jobId }) => {
                                   padding: "5px",
                                 }}
                               >
-                                <Radio.Group
-                                  onChange={onChange}
-                                  value={uiType}
-                                  size="small"
-                                >
-                                  <Radio.Button value="structured">
-                                    Structured
-                                  </Radio.Button>
-                                  <Radio.Button value="console">
-                                    Console
-                                  </Radio.Button>
+                                <Radio.Group onChange={onChange} value={uiType} size="small">
+                                  <Radio.Button value="structured">Structured</Radio.Button>
+                                  <Radio.Button value="console">Console</Radio.Button>
                                 </Radio.Group>
                               </div>
                               {uiType === "structured" ? (
@@ -388,26 +333,16 @@ export const DetailsJob = ({ jobId }) => {
               <Card
                 title={
                   <span style={{ fontSize: "14px" }}>
-                    <b>Needs Confirmation:</b> Someone from{" "}
-                    <b>{job.data.attributes.approvalTeam}</b> must confirm to
+                    <b>Needs Confirmation:</b> Someone from <b>{job.data.attributes.approvalTeam}</b> must confirm to
                     continue.
                   </span>
                 }
               >
                 <Space size={20}>
-                  <Button
-                    icon={<CheckOutlined />}
-                    onClick={handleApprove}
-                    type="primary"
-                  >
+                  <Button icon={<CheckOutlined />} onClick={handleApprove} type="primary">
                     Approve
                   </Button>
-                  <Button
-                    icon={<CloseOutlined />}
-                    onClick={handleRejected}
-                    type="primary"
-                    danger
-                  >
+                  <Button icon={<CloseOutlined />} onClick={handleRejected} type="primary" danger>
                     Discard
                   </Button>
                   <Button icon={<CommentOutlined />} onClick={handleComingSoon}>
@@ -420,24 +355,17 @@ export const DetailsJob = ({ jobId }) => {
             <span />
           )}
 
-          {job.data.attributes.status === "running" ||
-          job.data.attributes.status === "pending" ? (
+          {job.data.attributes.status === "running" || job.data.attributes.status === "pending" ? (
             <div style={{ margin: "auto", width: "50%", marginTop: "20px" }}>
               <Card
                 title={
                   <span style={{ fontSize: "14px" }}>
-                    <b>Cancelable:</b> You can cancel this job to stop it from
-                    executing.
+                    <b>Cancelable:</b> You can cancel this job to stop it from executing.
                   </span>
                 }
               >
                 <Space size={20}>
-                  <Button
-                    icon={<StopOutlined />}
-                    onClick={handleCancel}
-                    type="default"
-                    danger
-                  >
+                  <Button icon={<StopOutlined />} onClick={handleCancel} type="default" danger>
                     Cancel Job
                   </Button>
                   <Button icon={<CommentOutlined />} onClick={handleComingSoon}>
--- ui/src/domain/types.ts
+++ ui/src/domain/types.ts
@@ -1,3 +1,12 @@
+// Shated
+type AuditFieldBase = {
+  createdDate: string;
+  createdBy: string;
+  updatDate?: string;
+  updateBy?: string;
+};
+
+// Organization
 export type Organization = {
   id: string;
   attributes: OrganizationAttributes;
@@ -15,3 +24,57 @@ export type ApiResponse<T> = {
 export type FlatOrganization = {
   id: string;
 } & OrganizationAttributes;
+
+// Templates
+
+export type Template = {
+  id: string;
+  attributes: TemplateAttributes;
+};
+export type TemplateAttributes = {
+  name: string;
+};
+
+// Jobs
+export type Job = {
+  id: string;
+  attributes: JobAttributes;
+};
+
+export enum JobStatus {
+  Pending = "pending",
+  WaitingApproval = "waitingApproval",
+  Approved = "approved",
+  Queue = "queue",
+  Running = "running",
+  Completed = "completed",
+  NoChanges = "noChanges",
+  NotExecuted = "notExecuted",
+  Rejected = "rejected",
+  Cancelled = "cancelled",
+  Failed = "failed",
+  Unknown = "unknown",
+}
+export enum JobVia {
+  Ui = "UI",
+  Cli = "CLI",
+  Github = "Github",
+  Gitlab = "Gitlab",
+  Bitbucket = "Bitbucket",
+}
+
+export type JobAttributes = {
+  status: JobStatus;
+  via: JobVia;
+  output: string;
+  approvalTeam: string;
+} & AuditFieldBase;
+
+export type JobStep = {
+  id: string;
+  name: string;
+  stepNumber: number;
+  status: JobStatus;
+  output: string;
+  outputLog: string;
+};
