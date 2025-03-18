f4fc259f3811f1ec92042459c1d3f593f5d91774
Joachim Dalen <me@d3v.no>
2025-03-18 17:09:59 +0100

    fix line endings

--- ui/src/config/axiosConfig.ts
+++ ui/src/config/axiosConfig.ts
@@ -1,58 +1,56 @@
-import axios from 'axios';
+import axios from "axios";
 import getUserFromStorage from "./authUser";
 
 const axiosInstance = axios.create({
-    baseURL: window._env_.REACT_APP_TERRAKUBE_API_URL
+  baseURL: window._env_.REACT_APP_TERRAKUBE_API_URL,
 });
 
 export const axiosClient = axios.create({
-  baseURL: window._env_.REACT_APP_TERRAKUBE_API_URL
+  baseURL: window._env_.REACT_APP_TERRAKUBE_API_URL,
 });
 
 export const axiosClientAuth = axios.create({
-  baseURL: window._env_.REACT_APP_TERRAKUBE_API_URL
+  baseURL: window._env_.REACT_APP_TERRAKUBE_API_URL,
 });
-
 
 export const axiosGraphQL = axios.create({
-  baseURL: new URL(window._env_.REACT_APP_TERRAKUBE_API_URL).origin + "/graphql/api/v1"
+  baseURL: new URL(window._env_.REACT_APP_TERRAKUBE_API_URL).origin + "/graphql/api/v1",
 });
 
-
 axiosInstance.interceptors.request.use(
-  function(config) {
+  function (config) {
     const user = getUserFromStorage();
     const accessToken = user?.access_token;
     config.headers["Authorization"] = `Bearer ${accessToken}`;
     return config;
   },
-  function(error) {
+  function (error) {
     Promise.reject(error);
   }
-)
+);
 
 axiosGraphQL.interceptors.request.use(
-  function(config) {
+  function (config) {
     const user = getUserFromStorage();
     const accessToken = user?.access_token;
     config.headers["Authorization"] = `Bearer ${accessToken}`;
     return config;
   },
-  function(error) {
+  function (error) {
     Promise.reject(error);
   }
-)
+);
 
 axiosClientAuth.interceptors.request.use(
-  function(config) {
+  function (config) {
     const user = getUserFromStorage();
     const accessToken = user?.access_token;
     config.headers["Authorization"] = `Bearer ${accessToken}`;
     return config;
   },
-  function(error) {
+  function (error) {
     Promise.reject(error);
   }
-)
+);
 
 export default axiosInstance;
