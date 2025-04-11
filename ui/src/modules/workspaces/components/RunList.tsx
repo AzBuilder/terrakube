276354ee0f7b31930f72c393e8859335dfad96ae
Javier Canizalez <javier-canizalez@outlook.com>
2025-04-11 17:39:50 +0000

    improved format

--- ui/src/modules/workspaces/components/RunList.tsx
+++ ui/src/modules/workspaces/components/RunList.tsx
@@ -1,6 +1,13 @@
-import { List } from "antd";
-import { FlatJob } from "../../../domain/types";
-import RunCard from "./RunCard";
+import { List, Avatar, Tag } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import {
+  CheckCircleOutlined,
+  ClockCircleOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  SyncOutlined,
+} from "@ant-design/icons";
+import { FlatJob, JobStatus } from "../../../domain/types";
 
 type Props = {
   jobs: FlatJob[];
@@ -8,6 +15,31 @@ type Props = {
 };
 
 export default function RunList({ jobs, onRunClick }: Props) {
+  const renderStatusTag = (status: JobStatus, statusColor: string) => (
+    <Tag
+      icon={
+        status === JobStatus.Completed ? (
+          <CheckCircleOutlined />
+        ) : status === JobStatus.NoChanges ? (
+          <CheckCircleOutlined />
+        ) : status === JobStatus.Running ? (
+          <SyncOutlined spin />
+        ) : status === JobStatus.WaitingApproval ? (
+          <ExclamationCircleOutlined />
+        ) : status === JobStatus.Cancelled ? (
+          <StopOutlined />
+        ) : status === JobStatus.Failed ? (
+          <StopOutlined />
+        ) : (
+          <ClockCircleOutlined />
+        )
+      }
+      color={statusColor}
+    >
+      {status.toLowerCase()}
+    </Tag>
+  );
+
   return (
     <div>
       <h3>Run List</h3>
@@ -15,8 +47,25 @@ export default function RunList({ jobs, onRunClick }: Props) {
         itemLayout="horizontal"
         dataSource={jobs.sort((a, b) => parseInt(a.id) - parseInt(b.id)).reverse()}
         renderItem={(item) => (
-          <List.Item>
-            <RunCard item={item} onClick={onRunClick} />
+          <List.Item
+            actions={[
+              <div key="status" style={{ textAlign: 'right' }}>
+                {renderStatusTag(item.status, item.statusColor)}
+                <div>
+                  <span className="metadata">{item.latestChange}</span>
+                </div>
+              </div>
+            ]}
+          >
+            <List.Item.Meta
+              avatar={<Avatar shape="square" icon={<UserOutlined />} />}
+              title={<a onClick={() => onRunClick(item.id)}>{item.title}</a>}
+              description={
+                <span>
+                  #job-{item.id} | commitId {item.commitId?.substring(0, 6)} | {item.createdBy} triggered via {item.via || "UI"}
+                </span>
+              }
+            />
           </List.Item>
         )}
       />
