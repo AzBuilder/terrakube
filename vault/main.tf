cc5bd55057ca128b0e0afcbcddf834d86b64454f
Alfredo España <alfespa17@gmail.com>
2025-06-24 14:30:47 -0600

    dummy

--- vault/main.tf
+++ vault/main.tf
@@ -2,17 +2,12 @@ provider vault {
   address = "http://localhost:8200"
   skip_tls_verify = true
   skip_child_token = true
-  
-}
-
-data "vault_kv_secret_v2" "username" {
-  mount = "shared/data/kv/creds"
-  name  = "username"
-}
+  token = ""
+  }
 
 data "vault_kv_secret_v2" "password" {
-  mount = "shared/data/kv/creds"
-  name  = "password"
+  mount = "shared"
+  name  = "kv/creds"
 }
 
 
