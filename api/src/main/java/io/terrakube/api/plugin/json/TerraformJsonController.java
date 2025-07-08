877ebab45d6d5d0010fe04d109fad4b71f7f0b3f
Alfredo España <alfespa17@gmail.com>
2025-07-08 14:21:06 -0600

    feat: Implement web client for fetching Terraform index and add related tests

--- api/src/main/java/io/terrakube/api/plugin/json/TerraformJsonController.java
+++ api/src/main/java/io/terrakube/api/plugin/json/TerraformJsonController.java
@@ -8,10 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
-import java.net.URI;
-import java.nio.charset.Charset;
 
 @Slf4j
 @AllArgsConstructor
@@ -19,20 +19,42 @@ import java.nio.charset.Charset;
 @RequestMapping("/terraform")
 public class TerraformJsonController {
 
-    TerraformJsonProperties terraformJsonProperties;
+    private TerraformJsonProperties terraformJsonProperties;
+    private WebClient.Builder webClientBuilder;
 
     @GetMapping(value= "/index.json", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> createToken() throws IOException {
+
         String terraformIndex = "";
         if(terraformJsonProperties.getReleasesUrl() != null && !terraformJsonProperties.getReleasesUrl().isEmpty()) {
             log.info("Using terraform releases URL {}", terraformJsonProperties.getReleasesUrl());
-            terraformIndex = IOUtils.toString(URI.create(terraformJsonProperties.getReleasesUrl()), Charset.defaultCharset().toString());
+            terraformIndex = terraformJsonProperties.getReleasesUrl();
         } else {
-            String defaultUrl="https://releases.hashicorp.com/terraform/index.json";
-            log.warn("Using terraform releases URL {}", defaultUrl);
-            terraformIndex = IOUtils.toString(URI.create(defaultUrl), Charset.defaultCharset().toString());
+            log.warn("Using terraform releases URL https://releases.hashicorp.com/terraform/index.json");
+            terraformIndex = "https://releases.hashicorp.com/terraform/index.json";
         }
-        return new ResponseEntity<>(terraformIndex, HttpStatus.OK);
+
+        WebClient webClient = webClientBuilder
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024))
+                        .build())
+                .baseUrl(terraformIndex)
+                .build();
+
+        try {
+            terraformIndex = webClient.get()
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            return new ResponseEntity<>(terraformIndex, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(terraformIndex, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
     }
 }
 
